c81b9fda919ac52a6cf553c1fe0d614d2dacb628
suah <156158742+kimsuahh@users.noreply.github.com>
2024-09-24 10:57:28 +0900

    [feat] 메인페이지 화면구성
    메인 페이지 화면 구성, 강아지 선택 모달 구현

--- Front-End/dogbogam-front/src/App.tsx
+++ Front-End/dogbogam-front/src/App.tsx
@@ -1,12 +1,14 @@
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import HomePages from "./routes/HomePages";
 
 function App() {
   return (
     <>
       {/* 너비 360px, 높이 780px로 고정 */}
-      {/* pt-6 px-4 bg-white // status bar 크기 24px (pt-6), 양쪽 패딩 16px (px-4)으로 고정  */}
       <div className="w-[360px] h-[780px] relative">
-        <Routes>{/* <Route path="/" element={} /> */}</Routes>
+        <Routes>
+          <Route path="/home/*" element={<HomePages />} />
+        </Routes>
       </div>
     </>
   );
---
+++ Front-End/dogbogam-front/src/pages/Product/Insurance.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Insurance = () => {
+  return (
+    <div className="h-full pt-6 px-4 bg-white flex flex-col justify-between">
+      <div className="h-full flex flex-col">
+      </div>
+    </div>
+  );
+};
