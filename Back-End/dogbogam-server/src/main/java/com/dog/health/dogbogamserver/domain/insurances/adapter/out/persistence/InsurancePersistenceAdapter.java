292d890c206d4366efa8b7de08faadbc53f50d96
정재희 <wjfwogml5890@naver.com>
2024-09-25 15:17:53 +0900

    feat: 보험 상세 조회 어댑터 작성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurances/adapter/out/persistence/InsurancePersistenceAdapter.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurances/adapter/out/persistence/InsurancePersistenceAdapter.java
@@ -1,14 +1,17 @@
 package com.dog.health.dogbogamserver.domain.insurances.adapter.out.persistence;
 
 import com.dog.health.dogbogamserver.domain.insurances.application.port.out.FindAllInsurancePort;
+import com.dog.health.dogbogamserver.domain.insurances.application.port.out.FindDetailInsurancePort;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.*;
 
 @Repository
 @RequiredArgsConstructor
-public class InsurancePersistenceAdapter implements FindAllInsurancePort {
+public class InsurancePersistenceAdapter implements FindAllInsurancePort, FindDetailInsurancePort {
 
     private final InsuranceSpringDataRepository insuranceRepository;
     private final InsuranceBenefitMapper insuranceBenefitMapper;
@@ -24,6 +27,16 @@ public class InsurancePersistenceAdapter implements FindAllInsurancePort {
         }
 
         return insuranceBenefitMapper.insuranceEntityAndBenefitsToDomainList(insurances, insuranceBenefits);
+    }
+
+    @Override
+    public Map<String, Object> findByInsuranceId(Long insuranceId){
+        InsuranceEntity insurance = insuranceRepository.findById(insuranceId)
+                .orElseThrow(() -> new ChangeSetPersister.NotFoundException("요청한 보험이 존재하지 않습니다."));
+
+        List<InsuranceBenefitEntity> insuranceBenefits = insuranceBenefitRepository.findByInsuranceId(insuranceId);
+
+        return insuranceBenefits.insuranceBenefitEntityToDomain(insuranceBenefits);
     }
 
 }
