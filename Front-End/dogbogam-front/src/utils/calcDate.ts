76dcac893f84084ed83e4a5bd5466a342aab28e5
김준혁 <junhuk4079@gmail.com>
2024-09-25 10:22:34 +0900

    [FIX] 보험 납입 횟수 계산 로직 수정

--- Front-End/dogbogam-front/src/utils/calcDate.ts
+++ Front-End/dogbogam-front/src/utils/calcDate.ts
@@ -17,7 +17,7 @@ export const calSignUpDate = (createdTime: Date | null): number | null => {
   return diffDay;
 };
 
-// 경과 시간 확인하는 함수
+// 진료 경과 시간 확인하는 함수
 export const calRelativeTime = (date: Date): string | null => {
   const now = new Date();
   const diffInMs = now.getTime() - date.getTime();
@@ -34,4 +34,17 @@ export const calRelativeTime = (date: Date): string | null => {
   } else {
     return `${diffInDays}일 전`;
   }
+};
+
+// 보험 납입 횟수 계산하는 함수
+export const calPaymentsTime = (startDate: Date): number => {
+  const now = new Date();
+  // 년도 걔산
+  const yearDiff = now.getFullYear() - startDate.getFullYear();
+  // 월 계산
+  const monthDiff = now.getMonth() - startDate.getMonth();
+  // 차이 계산
+  const totalMonths = yearDiff * 12 + monthDiff;
+  // 납입 횟수 반환
+  return Math.max(totalMonths + 1, 1);
 };
