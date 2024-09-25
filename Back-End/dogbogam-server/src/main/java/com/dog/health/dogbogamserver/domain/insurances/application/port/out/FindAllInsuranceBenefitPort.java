9d6a0c3013b5be4838bc1488d7ed733087b932be
정재희 <wjfwogml5890@naver.com>
2024-09-25 17:15:22 +0900

    feat: 보장 혜택 조회 아웃 포트 작성

---
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurances/application/port/out/FindAllInsuranceBenefitPort.java
@@ -0,0 +1,9 @@
+package com.dog.health.dogbogamserver.domain.insurances.application.port.out;
+
+import java.util.List;
+
+public interface FindAllInsuranceBenefitPort {
+
+    List<String> findAllBenefits();
+
+}
