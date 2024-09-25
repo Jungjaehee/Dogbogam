8eb3d6954d245a2e81203b70a8a59aad5925db8a
정재희 <wjfwogml5890@naver.com>
2024-09-25 17:15:52 +0900

    feat: 보장 혜택 조회 서비스 작성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurances/application/service/InsuranceBenefitService.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurances/application/service/InsuranceBenefitService.java
@@ -1,6 +1,8 @@
 package com.dog.health.dogbogamserver.domain.insurances.application.service;
 
+import com.dog.health.dogbogamserver.domain.insurances.application.port.in.FindAllInsuranceBenefitUseCase;
 import com.dog.health.dogbogamserver.domain.insurances.application.port.in.SearchInsuranceUseCase;
+import com.dog.health.dogbogamserver.domain.insurances.application.port.out.FindAllInsuranceBenefitPort;
 import com.dog.health.dogbogamserver.domain.insurances.application.port.out.SearchInsurancePort;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,9 +14,10 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class InsuranceBenefitService implements SearchInsuranceUseCase {
+public class InsuranceBenefitService implements SearchInsuranceUseCase, FindAllInsuranceBenefitUseCase {
 
-    private final SearchInsurancePort insurancePort;
+    private final SearchInsurancePort searchInsurancePort;
+    private final FindAllInsuranceBenefitPort findAllInsuranceBenefitPort;
 
     @Override
     public List<Map<String, Object>> search(List<String> benefit) {
@@ -22,6 +25,11 @@ public class InsuranceBenefitService implements SearchInsuranceUseCase {
         if(benefit.size() == 0)
             throw new IllegalArgumentException("선택된 보장 혜택이 존재하지 않습니다.");
 
-        return insurancePort.findByBenefit(benefit);
+        return searchInsurancePort.findByBenefit(benefit);
+    }
+
+    @Override
+    public List<String> findAllBenefits(){
+        return findAllInsuranceBenefitPort.findAllBenefits();
     }
 }
