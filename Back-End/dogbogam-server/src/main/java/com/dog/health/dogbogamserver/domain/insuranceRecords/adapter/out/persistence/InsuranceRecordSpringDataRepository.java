f8692e83c47cd2b044531359afdbf9fd72eace24
Jungjaehee <wjfwogml5890@naver.com>
2024-09-26 16:47:08 +0900

    feat: 마이펫보험 repository 생성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecords/adapter/out/persistence/InsuranceRecordSpringDataRepository.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecords/adapter/out/persistence/InsuranceRecordSpringDataRepository.java
@@ -1,4 +1,9 @@
 package com.dog.health.dogbogamserver.domain.insuranceRecords.adapter.out.persistence;
 
-public interface InsuranceRecordSpringDataRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InsuranceRecordSpringDataRepository extends JpaRepository<InsuranceRecordEntity, Long> {
+    Optional<InsuranceRecordEntity> findByInsurance_InsuranceIdAndDog_DogId(Long insuranceId, Long dogId);
 }
