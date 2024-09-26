5c3be2679be71bf4fc5e79869159b04bfecb873b
jaehee Jung <wjfwogml5890@naver.com>
2024-09-26 13:22:08 +0900

    feat: 마이펫보험 도메인 작성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecords/domain/InsuranceRecord.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecords/domain/InsuranceRecord.java
@@ -1,4 +1,21 @@
 package com.dog.health.dogbogamserver.domain.insuranceRecords.domain;
 
+import com.dog.health.dogbogamserver.domain.dogs.domain.Dog;
+import com.dog.health.dogbogamserver.domain.insurances.domain.Insurance;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class InsuranceRecord {
+
+    private Long insuranceRecordId;
+    private Insurance insurance;
+    private Dog dog;
+    private LocalDate registDate;
+    private Long monthlyPayment;
+    private LocalDate expirationDate;
+    private boolean isDeleted;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
 }
