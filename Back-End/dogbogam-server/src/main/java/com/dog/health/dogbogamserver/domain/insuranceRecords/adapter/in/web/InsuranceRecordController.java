766161770eb7b012b926bbf32a0e7efdd9047a55
Jungjaehee <wjfwogml5890@naver.com>
2024-09-26 16:47:35 +0900

    feat: 마이펫보험 등록 controller 작성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecords/adapter/in/web/InsuranceRecordController.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecords/adapter/in/web/InsuranceRecordController.java
@@ -1,4 +1,27 @@
 package com.dog.health.dogbogamserver.domain.insuranceRecords.adapter.in.web;
 
+import com.dog.health.dogbogamserver.domain.insuranceRecords.adapter.in.dto.RegistRequestDto;
+import com.dog.health.dogbogamserver.domain.insuranceRecords.application.port.in.RegistInsuranceRecordUseCase;
+import com.dog.health.dogbogamserver.global.web.dto.response.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("insurance-records")
 public class InsuranceRecordController {
+
+    private final RegistInsuranceRecordUseCase registInsuranceRecordUseCase;
+
+    @PostMapping
+    public SuccessResponse<?> saveInsuranceRecord(@RequestBody final RegistRequestDto registRequestDto) {
+
+        registInsuranceRecordUseCase.registInsuranceRecord(registRequestDto);
+
+        return SuccessResponse.created();
+    }
+
 }
