b6e0c504b0fbbe0b0172b4cec107d3a0a57ebd27
Jungjaehee <wjfwogml5890@naver.com>
2024-09-26 16:48:02 +0900

    feat: 마이펫보험 toDomain 함수 작성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecords/adapter/out/persistence/InsuranceRecordMapper.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecords/adapter/out/persistence/InsuranceRecordMapper.java
@@ -1,4 +1,31 @@
 package com.dog.health.dogbogamserver.domain.insuranceRecords.adapter.out.persistence;
 
+import com.dog.health.dogbogamserver.domain.dogs.adapter.out.persistence.DogMapper;
+import com.dog.health.dogbogamserver.domain.insuranceRecords.domain.InsuranceRecord;
+import com.dog.health.dogbogamserver.domain.insurances.adapter.out.persistence.InsuranceMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class InsuranceRecordMapper {
+
+    private final InsuranceMapper insuranceMapper;
+    private final DogMapper dogMapper;
+
+    public InsuranceRecord toDomain(InsuranceRecordEntity insuranceRecordEntity){
+        return new InsuranceRecord(insuranceRecordEntity.getInsuranceRecordId(),
+                insuranceMapper.toDomain(insuranceRecordEntity.getInsurance()),
+                dogMapper.toDomain(insuranceRecordEntity.getDog()),
+                insuranceRecordEntity.getRegistDate(),
+                insuranceRecordEntity.getMonthlyPayment(),
+                insuranceRecordEntity.getExpirationDate(),
+                insuranceRecordEntity.isDeleted(),
+                insuranceRecordEntity.getCreatedAt(),
+                insuranceRecordEntity.getModifiedAt());
+    }
+
+    public InsuranceRecordEntity toEntity(InsuranceRecord insuranceRecord){
+
+    }
 }
