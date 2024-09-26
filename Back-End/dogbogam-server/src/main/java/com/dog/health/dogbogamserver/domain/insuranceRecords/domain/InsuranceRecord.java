9044e604f9727dfedbd6c01af4f961b8edc38761
Jungjaehee <wjfwogml5890@naver.com>
2024-09-26 16:46:14 +0900

    feat: 마이펫보험 도메인 생성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecords/domain/InsuranceRecord.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecords/domain/InsuranceRecord.java
@@ -2,10 +2,16 @@ package com.dog.health.dogbogamserver.domain.insuranceRecords.domain;
 
 import com.dog.health.dogbogamserver.domain.dogs.domain.Dog;
 import com.dog.health.dogbogamserver.domain.insurances.domain.Insurance;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class InsuranceRecord {
 
     private Long insuranceRecordId;
