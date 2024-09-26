db6850e7a750a6dbc140d35120327db1d7c0daf8
Jungjaehee <wjfwogml5890@naver.com>
2024-09-26 16:46:25 +0900

    feat: 마이펫보험 엔티티 생성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecords/adapter/out/persistence/InsuranceRecordEntity.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecords/adapter/out/persistence/InsuranceRecordEntity.java
@@ -1,4 +1,46 @@
 package com.dog.health.dogbogamserver.domain.insuranceRecords.adapter.out.persistence;
 
-public class InsuranceRecordEntity {
+import com.dog.health.dogbogamserver.domain.dogs.adapter.out.persistence.DogEntity;
+import com.dog.health.dogbogamserver.domain.insurances.adapter.out.persistence.InsuranceEntity;
+import com.dog.health.dogbogamserver.global.baseTimeEntity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "insurance_record")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InsuranceRecordEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long insuranceRecordId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "insurance_id", nullable = false)
+    private InsuranceEntity insurance;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dog_id", nullable = false)
+    private DogEntity dog;
+
+    @Column(name = "regist_date", nullable = false)
+    private LocalDate registDate;
+
+    @Column(name = "monthly_payment", nullable = false)
+    private Long monthlyPayment;
+
+    @Column(name = "expiration_date", nullable = false)
+    private LocalDate expirationDate;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
+
 }
