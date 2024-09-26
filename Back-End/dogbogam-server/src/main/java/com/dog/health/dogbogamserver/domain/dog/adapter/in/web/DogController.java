c5d1cb1ae6d945b1cdae8c723ed456f27e6a2c96
장현준 <50596733+gapple95@users.noreply.github.com>
2024-09-26 14:01:16 +0900

    feat: Controller 작성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/dog/adapter/in/web/DogController.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/dog/adapter/in/web/DogController.java
@@ -1,31 +1,48 @@
 package com.dog.health.dogbogamserver.domain.dog.adapter.in.web;
 
+import com.dog.health.dogbogamserver.domain.dog.application.port.in.CreateDogUseCase;
+import com.dog.health.dogbogamserver.domain.dog.application.port.in.UpdateDogUseCase;
+import com.dog.health.dogbogamserver.domain.dog.application.port.in.DeleteDogUseCase;
+import com.dog.health.dogbogamserver.domain.dog.application.port.in.FindDogDetailsUseCase;
 import com.dog.health.dogbogamserver.domain.dog.domain.Dog;
+import com.dog.health.dogbogamserver.global.web.dto.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/dogs")
 @RequiredArgsConstructor
 public class DogController {
-    private final DogUseCase dogUseCase;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Dog> getDogById(@PathVariable Long id) {
-        return ResponseEntity.ok(dogUseCase.getDogById(id));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Dog>> getAllDogs() {
-        return ResponseEntity.ok(dogUseCase.getAllDogs());
-    }
+    private final CreateDogUseCase createDogUseCase;
+    private final UpdateDogUseCase updateDogUseCase;
+    private final DeleteDogUseCase deleteDogUseCase;
+    private final FindDogDetailsUseCase findDogDetailsUseCase;
 
     @PostMapping
-    public ResponseEntity<Void> registerDog(@RequestBody Dog dog) {
-        dogUseCase.registerDog(dog);
-        return ResponseEntity.ok().build();
+    public SuccessResponse<?> createDog(@RequestBody Dog dog) {
+        createDogUseCase.createDog(dog);
+        return SuccessResponse.created();
+    }
+
+    @PutMapping("/{id}")
+    public SuccessResponse<?> updateDog(@PathVariable Long id, @RequestBody Dog dog) {
+        updateDogUseCase.updateDog(id, dog);
+        return SuccessResponse.updated();
+    }
+
+    @DeleteMapping("/{id}")
+    public SuccessResponse<?> deleteDog(@PathVariable Long id) {
+        deleteDogUseCase.deleteDog(id);
+        return SuccessResponse.deleted();
+    }
+
+    @GetMapping("/{id}")
+    public SuccessResponse<?> getDogDetails(@PathVariable Long id) {
+        Optional<Dog> dog = findDogDetailsUseCase.findDogDetails(id);
+        return dog.map(SuccessResponse::ok)
+                .orElseGet(() -> SuccessResponse.ok(null));  // SuccessResponse.ok()로 처리
     }
 }
