00769d63f7630f0948fe80523e50a9f320a3c30f
김수영 <kswim8914@gmail.com>
2024-09-27 13:45:52 +0900

    feat: 회원가입 메소드 구현

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/member/adapter/in/web/MemberController.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/member/adapter/in/web/MemberController.java
@@ -1,4 +1,28 @@
 package com.dog.health.dogbogamserver.domain.member.adapter.in.web;
 
+import com.dog.health.dogbogamserver.domain.member.application.port.in.RegisterMemberUseCase;
+import com.dog.health.dogbogamserver.domain.member.application.service.MemberService;
+import com.dog.health.dogbogamserver.domain.member.application.service.dto.request.CreateRequest;
+import com.dog.health.dogbogamserver.domain.member.application.service.dto.response.LoginResponse;
+import com.dog.health.dogbogamserver.global.web.dto.response.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final RegisterMemberUseCase registerMemberUseCase;
+
+    // 회원가입
+    @PostMapping
+    public SuccessResponse<LoginResponse> createMember(@RequestBody CreateRequest createRequest){
+        LoginResponse loginResponse = registerMemberUseCase.registerMember(createRequest);
+        return SuccessResponse.created(loginResponse);
+    }
 }
