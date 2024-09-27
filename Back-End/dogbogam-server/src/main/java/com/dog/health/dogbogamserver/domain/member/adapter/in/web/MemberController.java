07b837aa98b8126c45892e598a9303ff1c0205d0
김수영 <kswim8914@gmail.com>
2024-09-27 23:05:45 +0900

    feat: 로그인 메소드 추가

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/member/adapter/in/web/MemberController.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/member/adapter/in/web/MemberController.java
@@ -1,12 +1,12 @@
 package com.dog.health.dogbogamserver.domain.member.adapter.in.web;
 
+import com.dog.health.dogbogamserver.domain.member.application.port.in.LoginUseCase;
 import com.dog.health.dogbogamserver.domain.member.application.port.in.RegisterMemberUseCase;
-import com.dog.health.dogbogamserver.domain.member.application.service.MemberService;
 import com.dog.health.dogbogamserver.domain.member.application.service.dto.request.CreateRequest;
+import com.dog.health.dogbogamserver.domain.member.application.service.dto.request.LoginRequest;
 import com.dog.health.dogbogamserver.domain.member.application.service.dto.response.LoginResponse;
 import com.dog.health.dogbogamserver.global.web.dto.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final RegisterMemberUseCase registerMemberUseCase;
+    private final LoginUseCase loginUseCase;
 
     // 회원가입
-    @PostMapping
+    @PostMapping()
     public SuccessResponse<LoginResponse> createMember(@RequestBody CreateRequest createRequest){
         LoginResponse loginResponse = registerMemberUseCase.registerMember(createRequest);
         return SuccessResponse.created(loginResponse);
+    }
+
+    // 로그인
+    @PostMapping("/login")
+    public SuccessResponse<LoginResponse> login(@RequestBody LoginRequest loginRequest){
+        LoginResponse loginResponse = loginUseCase.login(loginRequest);
+        return SuccessResponse.ok(loginResponse);
     }
 }
