e373f15aa68233e202b314db0bfe2068b698e946
김수영 <kswim8914@gmail.com>
2024-09-27 13:37:38 +0900

    feat: registerMember 메소드 생성

---
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/member/application/port/in/RegisterMemberUseCase.java
@@ -0,0 +1,10 @@
+package com.dog.health.dogbogamserver.domain.member.application.port.in;
+
+import com.dog.health.dogbogamserver.domain.member.application.service.dto.request.CreateRequest;
+import com.dog.health.dogbogamserver.domain.member.application.service.dto.response.LoginResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface RegisterMemberUseCase {
+    LoginResponse registerMember(CreateRequest request);
+}
