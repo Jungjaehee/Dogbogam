0e88d7e27deb92d1ca5dace1fef9b50a70374348
정재희 <wjfwogml5890@naver.com>
2024-09-27 14:26:07 +0900

    refactor: 보험 검색 아웃 포트 반환타입 수정

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurance/application/port/out/SearchInsurancePort.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurance/application/port/out/SearchInsurancePort.java
@@ -1,9 +1,11 @@
 package com.dog.health.dogbogamserver.domain.insurance.application.port.out;
 
+import com.dog.health.dogbogamserver.domain.insurance.domain.InsuranceBenefit;
+
 import java.util.List;
 import java.util.Map;
 
 
 public interface SearchInsurancePort {
-    List<Map<String, Object>> findByBenefit(List<String> benefit);
+    List<InsuranceBenefit> findByBenefit(List<String> benefit);
 }
