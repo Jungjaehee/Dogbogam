4ba05edc94be4428b99359deaaec467183d4f80f
김수영 <kswim8914@gmail.com>
2024-09-27 13:06:50 +0900

    feat: Enum 형식으로 커스텀 예외 종류 추가

---
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/global/web/exception/ErrorCode.java
@@ -0,0 +1,21 @@
+package com.dog.health.dogbogamserver.global.web.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+    USER_NOT_FOUND("존재하지 않는 유저입니다.", HttpStatus.UNAUTHORIZED),
+    INVALID_PASSWORD("올바르지 않은 비밀번호입니다.", HttpStatus.UNAUTHORIZED),
+    INVALID_ACCESS_TOKEN("올바르지 않은 ACCESSTOKEN 입니다", HttpStatus.UNAUTHORIZED),
+    JSON_PROCESSING_ERROR("JSON 처리 중 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),;
+
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    ErrorCode(String message, HttpStatus httpStatus) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
+}
