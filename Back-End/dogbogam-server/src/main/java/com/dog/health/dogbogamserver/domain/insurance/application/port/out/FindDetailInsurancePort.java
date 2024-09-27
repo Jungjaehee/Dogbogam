26c30393ec72054683efa1b9993b124b73eab61e
정재희 <wjfwogml5890@naver.com>
2024-09-27 14:25:35 +0900

    refactor: 보험 상세 조회 아웃 포트에 보험 존재여부를 체크할 수 있는 함수 추가

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurance/application/port/out/FindDetailInsurancePort.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurance/application/port/out/FindDetailInsurancePort.java
@@ -1,9 +1,15 @@
 package com.dog.health.dogbogamserver.domain.insurance.application.port.out;
 
+import com.dog.health.dogbogamserver.domain.insurance.domain.Insurance;
+import com.dog.health.dogbogamserver.domain.insurance.domain.InsuranceBenefit;
+
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public interface FindDetailInsurancePort {
 
-    Map<String, Object> findByInsuranceId(Long insuranceId);
+    Optional<Insurance> existInsuranceById(Long insuranceId);
+    List<InsuranceBenefit> findByInsuranceId(Long insuranceId);
 
 }
