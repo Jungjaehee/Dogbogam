396445b980bcf998c91b957afabdc69251e856e6
정재희 <wjfwogml5890@naver.com>
2024-09-27 14:27:50 +0900

    refactor: 중복 의미 쿼리 삭제

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurance/adapter/out/persistence/InsuranceBenefitSpringDataRepository.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurance/adapter/out/persistence/InsuranceBenefitSpringDataRepository.java
@@ -1,17 +1,12 @@
 package com.dog.health.dogbogamserver.domain.insurance.adapter.out.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface InsuranceBenefitSpringDataRepository extends JpaRepository<InsuranceBenefitEntity, Long> {
 
-    @Query("SELECT DISTINCT ib.insurance FROM InsuranceBenefitEntity ib WHERE ib.benefit IN :benefits")
-    List<InsuranceEntity> findInsuranceEntityByBenefitIn(List<String> benefits);
-
-    @Query("SELECT ib.benefit FROM InsuranceBenefitEntity ib WHERE ib.insurance.insuranceId = :insuranceId")
-    List<String> findBenefitByInsuranceId(Long insuranceId);
+    List<InsuranceBenefitEntity> findByBenefitIn(List<String> benefits);
 
     List<InsuranceBenefitEntity> findByInsurance_InsuranceId(Long insuranceId);
 }
