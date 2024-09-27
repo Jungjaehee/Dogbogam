110631bada499ca8e8a982d86708731bd42b550b
김수영 <kswim8914@gmail.com>
2024-09-27 13:36:59 +0900

    feat: findByMemberId 생성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/member/adapter/out/persistence/MemberSpringDataRepository.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/member/adapter/out/persistence/MemberSpringDataRepository.java
@@ -2,5 +2,8 @@ package com.dog.health.dogbogamserver.domain.member.adapter.out.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberSpringDataRepository extends JpaRepository<MemberEntity, Long> {
+    Optional<MemberEntity> findByMemberId(Long memberId);
 }
