e32be4f9f96d91d9737c37e6270324669b69c7b2
정재희 <wjfwogml5890@naver.com>
2024-09-27 14:27:18 +0900

    refactor: Entity와 Domain 변환을 수행하는 함수 제외 모두 삭제

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurance/adapter/out/persistence/InsuranceBenefitMapper.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurance/adapter/out/persistence/InsuranceBenefitMapper.java
@@ -5,9 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -23,21 +21,6 @@ public class InsuranceBenefitMapper {
         );
     }
 
-    public List<Map<String, Object>> insuranceEntityAndBenefitsToDomainList(List<InsuranceEntity> entityList, List<List<String>> benefits){
-        List<Map<String, Object>> insuranceInfo = new ArrayList<>();
-
-        int i = 0;
-        for (InsuranceEntity insuranceEntity : entityList){
-            Map<String, Object> insuranceMap = new HashMap<>();
-            insuranceMap.put("insurance", insuranceMapper.toDomain(insuranceEntity));
-            insuranceMap.put("benefits", benefits.get(i++));
-
-            insuranceInfo.add(insuranceMap);
-        }
-
-        return insuranceInfo;
-    }
-
     public List<InsuranceBenefit> entityListToDomainList(List<InsuranceBenefitEntity> entityList){
         List<InsuranceBenefit> insuranceBenefitList = new ArrayList<>();
         for (InsuranceBenefitEntity insuranceBenefitEntity : entityList){
@@ -45,21 +28,6 @@ public class InsuranceBenefitMapper {
         }
 
         return insuranceBenefitList;
-    }
-
-    public Map<String, Object> insuranceBenefitEntityListToDomain(List<InsuranceBenefitEntity> entityList){
-
-        Map<String, Object> insuranceBenfitListDomain = new HashMap<>();
-        insuranceBenfitListDomain.put("insurance", insuranceMapper.toDomain(entityList.get(0).getInsurance()));
-
-        List<String> benefits = new ArrayList<>();
-        for (InsuranceBenefitEntity insuranceBenefitEntity : entityList){
-            benefits.add(insuranceBenefitEntity.getBenefit());
-        }
-
-        insuranceBenfitListDomain.put("benefits",benefits);
-
-        return insuranceBenfitListDomain;
     }
 
 }
