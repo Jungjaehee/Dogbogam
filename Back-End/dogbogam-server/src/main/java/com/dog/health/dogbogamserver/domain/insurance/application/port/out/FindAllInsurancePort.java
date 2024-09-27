762c28d27a361bd7a747d48bb9cf3153f1ea663b
정재희 <wjfwogml5890@naver.com>
2024-09-27 14:24:11 +0900

    refactor: 보험 전체 리스트 조회 포트 안 함수명 수정 및 Domain 리스트로 반환 타입 수정

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurance/application/port/out/FindAllInsurancePort.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurance/application/port/out/FindAllInsurancePort.java
@@ -1,9 +1,10 @@
 package com.dog.health.dogbogamserver.domain.insurance.application.port.out;
 
+import com.dog.health.dogbogamserver.domain.insurance.domain.InsuranceBenefit;
+
 import java.util.List;
-import java.util.Map;
 
 public interface FindAllInsurancePort {
 
-    List<Map<String, Object>> findAll();
+    List<InsuranceBenefit> findAllInsurance();
 }
