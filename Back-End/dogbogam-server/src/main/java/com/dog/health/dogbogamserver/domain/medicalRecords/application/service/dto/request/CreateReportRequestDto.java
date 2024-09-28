824ab0bda42ab41aec734bd8fd9843ca00f63a1b
장현준 <50596733+gapple95@users.noreply.github.com>
2024-09-28 19:06:19 +0900

    refactor: requestDto 유효성 검사 추가

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/medicalRecords/application/service/dto/request/CreateReportRequestDto.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/medicalRecords/application/service/dto/request/CreateReportRequestDto.java
@@ -1,14 +1,20 @@
 package com.dog.health.dogbogamserver.domain.medicalRecords.application.service.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 import java.time.LocalDate;
 
 @Getter
 public class CreateReportRequestDto {
+    @NotNull(message = "반려견 아이디는 필수 입니다.")
     Long dogId;
+
+    @NotNull(message = "진료 날짜는 필수 입니다.")
     LocalDate recordDate;
     String content;
+
+    @NotNull(message = "병원 이름은 필수 입니다.")
     String hospital;
     // File
 }
--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/medicalRecords/application/service/dto/request/UpdateReportRequestDto.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/medicalRecords/application/service/dto/request/UpdateReportRequestDto.java
@@ -1,5 +1,6 @@
 package com.dog.health.dogbogamserver.domain.medicalRecords.application.service.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -8,10 +9,17 @@ import java.time.LocalDate;
 @Getter
 @ToString
 public class UpdateReportRequestDto {
+    @NotNull(message = "진료 기록 아이디는 필수 입니다.")
     Long reportId;
+
+    @NotNull(message = "반려견 아이디는 필수 입니다.")
     Long dogId;
+
+    @NotNull(message = "진료 날짜는 필수 입니다.")
     LocalDate recordDate;
     String content;
+
+    @NotNull(message = "병원 이름은 필수 입니다.")
     String hospital;
     // File
 }
