0f21c8fec1b502fb25823a5cbe193ca48fc6955c
장현준 <50596733+gapple95@users.noreply.github.com>
2024-09-29 02:59:20 +0900

    fix: requestDto 유효성 검증 추가

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/dog/adapter/in/web/dto/CreateDogDTO.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/dog/adapter/in/web/dto/CreateDogDTO.java
@@ -1,18 +1,24 @@
 package com.dog.health.dogbogamserver.domain.dog.adapter.in.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 import java.time.LocalDate;
 
 @Getter
 public class CreateDogDTO {
+    @NotBlank(message = "멤머는 필수 입니다.")
     private Long memberId;
+    @NotBlank(message = "반려견 이름은 필수 입니다.")
     private String name;
+    @NotBlank(message = "견종은 필수 입니다.")
     private String breed;
+    @NotBlank(message = "성별은 필수 입니다.")
     private String gender;
     private LocalDate birthDate;
     private Double weight;
     private Boolean isNeutered;
+    private boolean isDeleted;
     private String imageName;
     private String imageUrl;
 }
--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/dog/adapter/in/web/dto/UpdateDogDTO.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/dog/adapter/in/web/dto/UpdateDogDTO.java
@@ -1,15 +1,21 @@
 package com.dog.health.dogbogamserver.domain.dog.adapter.in.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 import java.time.LocalDate;
 
 @Getter
 public class UpdateDogDTO {
+    @NotBlank(message = "반려견은 필수 입니다.")
     private Long dogId;
+    @NotBlank(message = "멤버는 필수 입니다.")
+    private Long memberId;
+    @NotBlank(message = "반려견 이름은 필수 입니다.")
     private String name;
+    @NotBlank(message = "견종은 필수 입니다.")
     private String breed;
-    private String gender;
+    @NotBlank(message = "성별은 필수 입니다.")
     private LocalDate birthDate;
     private Double weight;
     private Boolean isNeutered;
