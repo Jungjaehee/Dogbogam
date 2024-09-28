f29efd307cc0b40c189904556823e8a7a534d737
Jungjaehee <wjfwogml5890@naver.com>
2024-09-29 04:18:18 +0900

    feat: 마이펫보험 전체 리스트 조회를 위한 페이지네이션 조회 함수 작성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecord/adapter/out/persistence/InsuranceRecordSpringDataRepository.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecord/adapter/out/persistence/InsuranceRecordSpringDataRepository.java
@@ -1,9 +1,12 @@
 package com.dog.health.dogbogamserver.domain.insuranceRecord.adapter.out.persistence;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface InsuranceRecordSpringDataRepository extends JpaRepository<InsuranceRecordEntity, Long> {
     Optional<InsuranceRecordEntity> findByInsurance_InsuranceIdAndDog_DogId(Long insuranceId, Long dogId);
+    Page<InsuranceRecordEntity> findAllByDog_DogIdOrderByRegistDate(Long dogId, Pageable pageable);
 }
