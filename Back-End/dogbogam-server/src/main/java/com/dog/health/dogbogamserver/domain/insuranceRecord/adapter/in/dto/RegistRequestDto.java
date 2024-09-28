a2864eacfa5260d871e3628db4312664864b0a21
Jungjaehee <wjfwogml5890@naver.com>
2024-09-29 01:54:57 +0900

    feat: 등록 requestDto 유효성 검사 추가

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecord/adapter/in/dto/RegistRequestDto.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecord/adapter/in/dto/RegistRequestDto.java
@@ -1,6 +1,6 @@
 package com.dog.health.dogbogamserver.domain.insuranceRecord.adapter.in.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,19 +10,23 @@ import java.time.LocalDate;
 @Getter
 public class RegistRequestDto {
 
-    @NotNull
+    @NotNull(message = "보험 Id가 비어있습니다.")
     private Long insuranceId;
 
-    @NotNull
+    @NotNull(message = "반려견 Id가 비어있습니다.")
     private Long dogId;
 
-    @NotNull
+
+    @NotNull(message = "보험 등록 날짜가 비어있습니다.")
+    @PastOrPresent(message = "보험 등록 날짜는 오늘 이전만 가능합니다.")
     private LocalDate registDate;
 
-    @NotNull
+    @NotNull(message = "월 납입료가 비어있습니다.")
+    @Positive(message = "월 납입료 0원 이하는 불가능합니다.")
     private Long monthlyPayment;
 
-    @NotNull
+    @NotNull(message = "보험 만료 기간이 비어있습니다.")
+    @FutureOrPresent(message = "보험 만료 날짜는 오늘 이후만 가능합니다.")
     private LocalDate expirationDate;
 
 }
