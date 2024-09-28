bf601ac4284fddcdbc6c99422e20250c581094c8
Jungjaehee <wjfwogml5890@naver.com>
2024-09-29 02:58:57 +0900

    feat: 마이펫보험 삭제를 위한 컨트롤러 함수 추가

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecord/adapter/in/web/InsuranceRecordController.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecord/adapter/in/web/InsuranceRecordController.java
@@ -2,6 +2,7 @@ package com.dog.health.dogbogamserver.domain.insuranceRecord.adapter.in.web;
 
 import com.dog.health.dogbogamserver.domain.insuranceRecord.adapter.in.dto.RegistRequestDto;
 import com.dog.health.dogbogamserver.domain.insuranceRecord.adapter.in.dto.UpdateRequestDto;
+import com.dog.health.dogbogamserver.domain.insuranceRecord.application.port.in.DeleteInsuranceRecordUseCase;
 import com.dog.health.dogbogamserver.domain.insuranceRecord.application.port.in.FindInsuranceRecordUseCase;
 import com.dog.health.dogbogamserver.domain.insuranceRecord.application.port.in.RegistInsuranceRecordUseCase;
 import com.dog.health.dogbogamserver.domain.insuranceRecord.application.port.in.UpdateInsuranceRecordUseCase;
@@ -17,10 +18,10 @@ public class InsuranceRecordController {
     private final RegistInsuranceRecordUseCase registInsuranceRecordUseCase;
     private final UpdateInsuranceRecordUseCase updateInsuranceRecordUseCase;
     private final FindInsuranceRecordUseCase findInsuranceRecordUseCase;
+    private final DeleteInsuranceRecordUseCase deleteInsuranceRecordUseCase;
 
     @PostMapping
     public SuccessResponse<?> saveInsuranceRecord(@RequestBody final RegistRequestDto registRequestDto) {
-
         registInsuranceRecordUseCase.registInsuranceRecord(registRequestDto);
 
         return SuccessResponse.created();
@@ -34,8 +35,15 @@ public class InsuranceRecordController {
     }
 
     @GetMapping("/{insuranceRecordId}")
-    public SuccessResponse<?> findInsuranceRecord(@PathVariable("insuranceId") Long insuranceRecordId) {
+    public SuccessResponse<?> findInsuranceRecord(@PathVariable("insuranceRecordId") Long insuranceRecordId) {
         return SuccessResponse.ok(findInsuranceRecordUseCase.findInsuranceRecordById(insuranceRecordId));
     }
 
+
+    @DeleteMapping("/{insuranceRecordId")
+    public SuccessResponse<?> deleteInsuranceRecord(@PathVariable("insuranceRecordId")Long insuranceRecordId) {
+        deleteInsuranceRecordUseCase.deleteInsuranceRecord(insuranceRecordId);
+
+        return SuccessResponse.deleted();
+    }
 }
