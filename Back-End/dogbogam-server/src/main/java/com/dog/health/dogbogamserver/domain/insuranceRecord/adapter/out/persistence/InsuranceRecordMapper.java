c7ee777b5cc387b11c772ea9fa535d45397d7b68
Jungjaehee <wjfwogml5890@naver.com>
2024-09-29 04:14:48 +0900

    fix: Optional mapper 함수 삭제

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecord/adapter/out/persistence/InsuranceRecordMapper.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecord/adapter/out/persistence/InsuranceRecordMapper.java
@@ -6,8 +6,6 @@ import com.dog.health.dogbogamserver.domain.insurance.adapter.out.persistence.In
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 @Component
 @RequiredArgsConstructor
 public class InsuranceRecordMapper {
@@ -25,18 +23,6 @@ public class InsuranceRecordMapper {
                 insuranceRecordEntity.isDeleted(),
                 insuranceRecordEntity.getCreatedAt(),
                 insuranceRecordEntity.getModifiedAt());
-    }
-
-    public Optional<InsuranceRecord> toOptionalDomain(Optional<InsuranceRecordEntity> insuranceRecordEntity){
-        return insuranceRecordEntity.map(e -> new InsuranceRecord(e.getInsuranceRecordId(),
-                insuranceMapper.toDomain(e.getInsurance()),
-                dogMapper.toDomain(e.getDog()),
-                e.getRegistDate(),
-                e.getMonthlyPayment(),
-                e.getExpirationDate(),
-                e.isDeleted(),
-                e.getCreatedAt(),
-                e.getModifiedAt()));
     }
 
     public InsuranceRecordEntity toEntity(InsuranceRecord insuranceRecord){
