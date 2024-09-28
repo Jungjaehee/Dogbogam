c2d5eec2d71589684eacb39f54d48f466128189c
Jungjaehee <wjfwogml5890@naver.com>
2024-09-29 01:53:43 +0900

    feat: 마이펫보험 기록 등록을 위한 adapter 작성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecord/adapter/out/persistence/InsuranceRecordPersistenceAdapter.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecord/adapter/out/persistence/InsuranceRecordPersistenceAdapter.java
@@ -1,7 +1,10 @@
 package com.dog.health.dogbogamserver.domain.insuranceRecord.adapter.out.persistence;
 
+import com.dog.health.dogbogamserver.domain.dog.adapter.out.persistence.DogEntity;
+import com.dog.health.dogbogamserver.domain.dog.adapter.out.persistence.DogMapper;
 import com.dog.health.dogbogamserver.domain.dog.adapter.out.persistence.DogSpringDataRepository;
 import com.dog.health.dogbogamserver.domain.dog.domain.Dog;
+import com.dog.health.dogbogamserver.domain.insurance.adapter.out.persistence.InsuranceMapper;
 import com.dog.health.dogbogamserver.domain.insuranceRecord.application.port.out.SaveInsuranceRecordPort;
 import com.dog.health.dogbogamserver.domain.insuranceRecord.domain.InsuranceRecord;
 import com.dog.health.dogbogamserver.domain.insurance.adapter.out.persistence.InsuranceSpringDataRepository;
@@ -16,28 +19,32 @@ import java.util.Optional;
 public class InsuranceRecordPersistenceAdapter implements SaveInsuranceRecordPort {
 
     private final InsuranceRecordMapper insuranceRecordMapper;
+    private final InsuranceMapper insuranceMapper;
+    private final DogMapper dogMapper;
     private final InsuranceRecordSpringDataRepository insuranceRecordRepository;
     private final DogSpringDataRepository dogRepository;
     private final InsuranceSpringDataRepository insuranceRepository;
 
     @Override
     public void save(InsuranceRecord insuranceRecord) {
-
+        InsuranceRecordEntity insuranceRecordEntity = insuranceRecordMapper.toEntity(insuranceRecord);
+        insuranceRecordRepository.save(insuranceRecordEntity);
     }
 
     @Override
     public Optional<InsuranceRecord> checkExistingInsuranceRecord(Long dogId, Long insuranceId){
-        insuranceRecordRepository.findByInsurance_InsuranceIdAndDog_DogId(insuranceId, dogId);
+        return insuranceRecordMapper.toOptionalDomain(insuranceRecordRepository.findByInsurance_InsuranceIdAndDog_DogId(insuranceId, dogId));
     }
 
     @Override
     public Optional<Insurance> checkExistingInsurance(Long insuranceId){
-
+        return insuranceMapper.toOptionalDomain(insuranceRepository.findByInsuranceId(insuranceId));
     }
 
     @Override
-    public Optional<Dog> checkExistingDog(Long dogId){
-
+    public Dog checkExistingDog(Long dogId){
+        DogEntity dogEntity = dogRepository.findById(dogId).orElseThrow(() -> new IllegalArgumentException("해당 반려견이 존재하지 않습니다."));
+        return dogMapper.toDomain(dogEntity);
     }
 
 }
