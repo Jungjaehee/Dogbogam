c2c750ee57976a4b839164ede2ae99832c051534
Jungjaehee <wjfwogml5890@naver.com>
2024-09-29 04:18:44 +0900

    feat: 마이펫보험 전체 리스트 조회를 위한 어댑터 작성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecord/adapter/out/persistence/InsuranceRecordPersistenceAdapter.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insuranceRecord/adapter/out/persistence/InsuranceRecordPersistenceAdapter.java
@@ -4,20 +4,28 @@ import com.dog.health.dogbogamserver.domain.dog.adapter.out.persistence.DogEntit
 import com.dog.health.dogbogamserver.domain.dog.adapter.out.persistence.DogMapper;
 import com.dog.health.dogbogamserver.domain.dog.adapter.out.persistence.DogSpringDataRepository;
 import com.dog.health.dogbogamserver.domain.dog.domain.Dog;
+import com.dog.health.dogbogamserver.domain.insurance.adapter.out.persistence.InsuranceEntity;
 import com.dog.health.dogbogamserver.domain.insurance.adapter.out.persistence.InsuranceMapper;
 import com.dog.health.dogbogamserver.domain.insuranceRecord.application.port.out.LoadInsuranceRecordPort;
+import com.dog.health.dogbogamserver.domain.insuranceRecord.application.port.out.PageableLoadInsuranceRecordPort;
 import com.dog.health.dogbogamserver.domain.insuranceRecord.application.port.out.SaveInsuranceRecordPort;
 import com.dog.health.dogbogamserver.domain.insuranceRecord.domain.InsuranceRecord;
 import com.dog.health.dogbogamserver.domain.insurance.adapter.out.persistence.InsuranceSpringDataRepository;
 import com.dog.health.dogbogamserver.domain.insurance.domain.Insurance;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
-public class InsuranceRecordPersistenceAdapter implements SaveInsuranceRecordPort, LoadInsuranceRecordPort {
+@Transactional(readOnly = true)
+public class InsuranceRecordPersistenceAdapter implements SaveInsuranceRecordPort, LoadInsuranceRecordPort,
+        PageableLoadInsuranceRecordPort {
 
     private final InsuranceRecordMapper insuranceRecordMapper;
     private final InsuranceMapper insuranceMapper;
@@ -26,6 +34,7 @@ public class InsuranceRecordPersistenceAdapter implements SaveInsuranceRecordPor
     private final DogSpringDataRepository dogRepository;
     private final InsuranceSpringDataRepository insuranceRepository;
 
+    @Transactional
     @Override
     public void save(InsuranceRecord insuranceRecord) {
         InsuranceRecordEntity insuranceRecordEntity = insuranceRecordMapper.toEntity(insuranceRecord);
@@ -34,23 +43,35 @@ public class InsuranceRecordPersistenceAdapter implements SaveInsuranceRecordPor
 
     @Override
     public Optional<InsuranceRecord> checkExistingInsuranceRecord(Long dogId, Long insuranceId){
-        return insuranceRecordMapper.toOptionalDomain(insuranceRecordRepository.findByInsurance_InsuranceIdAndDog_DogId(insuranceId, dogId));
+        Optional<InsuranceRecordEntity> insuranceRecordEntity =
+                insuranceRecordRepository.findByInsurance_InsuranceIdAndDog_DogId(insuranceId, dogId);
+        return insuranceRecordEntity.map(insuranceRecordMapper::toDomain);
     }
 
     @Override
     public Optional<Insurance> checkExistingInsurance(Long insuranceId){
-        return insuranceMapper.toOptionalDomain(insuranceRepository.findByInsuranceId(insuranceId));
+        Optional<InsuranceEntity> insuranceEntity = insuranceRepository.findByInsuranceId(insuranceId);
+        return insuranceEntity.map(insuranceMapper::toDomain);
     }
 
     @Override
-    public Dog checkExistingDog(Long dogId){
-        DogEntity dogEntity = dogRepository.findById(dogId).orElseThrow(() -> new IllegalArgumentException("해당 반려견이 존재하지 않습니다."));
-        return dogMapper.toDomain(dogEntity);
+    public Optional<Dog> checkExistingDog(Long dogId){
+        Optional<DogEntity> dogEntity = dogRepository.findById(dogId);
+        return dogEntity.map(dogMapper::toDomain);
     }
 
     @Override
     public Optional<InsuranceRecord> loadInsuranceRecord(Long insuranceRecordId){
-        return insuranceRecordMapper.toOptionalDomain(insuranceRecordRepository.findById(insuranceRecordId));
+        Optional<InsuranceRecordEntity> insuranceRecordEntity = insuranceRecordRepository.findById(insuranceRecordId);
+        return insuranceRecordEntity.map(insuranceRecordMapper::toDomain);
     }
 
+    @Override
+    public Page<InsuranceRecord> findAllInsuranceRecords(Long dogId, int size, int page){
+        Pageable pageable = PageRequest.of(--page, size);
+        Page<InsuranceRecordEntity> insuranceRecordEntityPage = insuranceRecordRepository
+                .findAllByDog_DogIdOrderByRegistDate(dogId, pageable);
+
+        return insuranceRecordEntityPage.map(insuranceRecordMapper::toDomain);
+    }
 }
