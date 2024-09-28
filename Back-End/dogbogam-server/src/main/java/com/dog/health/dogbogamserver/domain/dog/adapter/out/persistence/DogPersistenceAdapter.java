909a438a0e6eb62f23243b1f2cc2aa834ef9c856
장현준 <50596733+gapple95@users.noreply.github.com>
2024-09-29 02:50:48 +0900

    feat: 반려견 조회 리스트 Adapter 작성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/dog/adapter/out/persistence/DogPersistenceAdapter.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/dog/adapter/out/persistence/DogPersistenceAdapter.java
@@ -1,21 +1,25 @@
 package com.dog.health.dogbogamserver.domain.dog.adapter.out.persistence;
 
-import com.dog.health.dogbogamserver.domain.dog.adapter.in.web.dto.CreateDogDTO;
-import com.dog.health.dogbogamserver.domain.dog.application.port.out.CreateDogPort;
-import com.dog.health.dogbogamserver.domain.dog.application.port.out.UpdateDogPort;
-import com.dog.health.dogbogamserver.domain.dog.application.port.out.DeleteDogPort;
-import com.dog.health.dogbogamserver.domain.dog.application.port.out.FindDogDetailsPort;
+import com.dog.health.dogbogamserver.domain.dog.application.port.out.*;
 import com.dog.health.dogbogamserver.domain.dog.domain.Dog;
+import com.dog.health.dogbogamserver.domain.member.adapter.out.persistence.MemberEntity;
+import com.dog.health.dogbogamserver.domain.member.adapter.out.persistence.MemberSpringDataRepository;
+import com.dog.health.dogbogamserver.global.web.exception.CustomException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
+
+import static com.dog.health.dogbogamserver.global.web.exception.ErrorCode.USER_NOT_FOUND;
 
 @Component
 @RequiredArgsConstructor
-public class DogPersistenceAdapter implements CreateDogPort, UpdateDogPort, DeleteDogPort, FindDogDetailsPort {
+public class DogPersistenceAdapter implements CreateDogPort, UpdateDogPort, DeleteDogPort, FindDogDetailsPort,
+        FindDogsPort {
 
+    private final MemberSpringDataRepository memberSpringDataRepository;
     private final DogSpringDataRepository dogSpringDataRepository;
     private final DogMapper dogMapper;
 
@@ -47,5 +51,12 @@ public class DogPersistenceAdapter implements CreateDogPort, UpdateDogPort, Dele
     @Override
     public Optional<DogEntity> findEntityByDogId(Long dogId) {
         return dogSpringDataRepository.findById(dogId);
+    }
+
+    @Override
+    public Optional<List<Dog>> findDogsByMemberId(Long memberId) {
+        MemberEntity memberEntity = memberSpringDataRepository.findById(memberId).orElseThrow(()->new CustomException(USER_NOT_FOUND));
+        Optional<List<DogEntity>> dogList = dogSpringDataRepository.findDogsByMember(memberEntity);
+        return Optional.ofNullable(dogMapper.entityListToDomainList(dogList.get()));
     }
 }
