741dee9f6e459ae73c5c422aa31afe65cd590599
장현준 <50596733+gapple95@users.noreply.github.com>
2024-09-30 03:30:27 +0900

    feat: AI 진달 질병 리스트 조회 adapter, service 작성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/aiReportDisease/application/service/AiReportDiseaseService.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/aiReportDisease/application/service/AiReportDiseaseService.java
@@ -2,19 +2,25 @@ package com.dog.health.dogbogamserver.domain.aiReportDisease.application.service
 
 import com.dog.health.dogbogamserver.domain.aiReportDisease.application.port.in.CreateAiReportDiseaseUseCase;
 import com.dog.health.dogbogamserver.domain.aiReportDisease.application.port.in.FindAiReportDiseaseUseCase;
+import com.dog.health.dogbogamserver.domain.aiReportDisease.application.port.in.FindAiReportDiseasesUseCase;
 import com.dog.health.dogbogamserver.domain.aiReportDisease.application.port.out.CreateAiReportDiseasePort;
 import com.dog.health.dogbogamserver.domain.aiReportDisease.application.port.out.FindAiReportDiseasePort;
+import com.dog.health.dogbogamserver.domain.aiReportDisease.application.port.out.FindAiReportDiseasesPort;
 import com.dog.health.dogbogamserver.domain.aiReportDisease.application.service.dto.request.CreateAiReportDiseaseDto;
 import com.dog.health.dogbogamserver.domain.aiReportDisease.domain.AiReportDisease;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
-public class AiReportDiseaseService implements CreateAiReportDiseaseUseCase, FindAiReportDiseaseUseCase {
+public class AiReportDiseaseService implements CreateAiReportDiseaseUseCase, FindAiReportDiseaseUseCase,
+        FindAiReportDiseasesUseCase {
 
     private final CreateAiReportDiseasePort createAiReportDiseasePort;
     private final FindAiReportDiseasePort findAiReportDiseasePort;
+    private final FindAiReportDiseasesPort findAiReportDiseasesPort;
 
     @Override
     public void createAiReportDisease(CreateAiReportDiseaseDto requestDto) {
@@ -24,5 +30,10 @@ public class AiReportDiseaseService implements CreateAiReportDiseaseUseCase, Fin
     @Override
     public AiReportDisease findAiReportDiseaseById(Long id) {
         return findAiReportDiseasePort.findAiReportDiseaseById(id);
+    }
+
+    @Override
+    public List<AiReportDisease> findAiReportsByDiagnosisId(Long diagnosisId) {
+        return findAiReportDiseasesPort.findAiReportDiseasesByDiagnosisId(diagnosisId);
     }
 }
