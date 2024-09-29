0929661c67e1f7883be8796976c170709bf36630
장현준 <50596733+gapple95@users.noreply.github.com>
2024-09-30 03:17:44 +0900

    feat: AI 진달 질병 adapter, service 작성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/aiReportDisease/adapter/out/persistence/AiReportDiseasePersistenceAdapter.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/aiReportDisease/adapter/out/persistence/AiReportDiseasePersistenceAdapter.java
@@ -5,19 +5,25 @@ import com.dog.health.dogbogamserver.domain.aiDiagnosis.adapter.out.persistence.
 import com.dog.health.dogbogamserver.domain.aiDiagnosis.adapter.out.persistence.AiDiagnosisPersistenceAdapter;
 import com.dog.health.dogbogamserver.domain.aiDiagnosis.domain.AiDiagnosis;
 import com.dog.health.dogbogamserver.domain.aiReportDisease.application.port.out.CreateAiReportDiseasePort;
+import com.dog.health.dogbogamserver.domain.aiReportDisease.application.port.out.FindAiReportDiseasePort;
 import com.dog.health.dogbogamserver.domain.aiReportDisease.application.service.dto.request.CreateAiReportDiseaseDto;
+import com.dog.health.dogbogamserver.domain.aiReportDisease.domain.AiReportDisease;
+import com.dog.health.dogbogamserver.global.web.exception.CustomException;
+import com.dog.health.dogbogamserver.global.web.exception.ErrorCode;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class AiReportDiseasePersistenceAdapter implements CreateAiReportDiseasePort {
+public class AiReportDiseasePersistenceAdapter implements CreateAiReportDiseasePort, FindAiReportDiseasePort {
 
     private final AiReportDiseaseMapper DiseaseMapper;
     private final AiReportDiseaseSpringDataRepository jpaRepository;
     private final AiDiagnosisPersistenceAdapter persistenceAdapter;
     private final AiDiagnosisMapper DiagnosisMapper;
+    private final AiDiagnosisMapper aiDiagnosisMapper;
+    private final AiReportDiseaseMapper aiReportDiseaseMapper;
 
     @Override
     @Transactional
@@ -31,5 +37,13 @@ public class AiReportDiseasePersistenceAdapter implements CreateAiReportDiseaseP
                 .percentage(requestDto.getPercentage())
                 .build();
         jpaRepository.save(aiReportDiseaseEntity);
+    }
+
+    @Override
+    public AiReportDisease findAiReportDiseaseById(Long id) {
+        AiReportDiseaseEntity entity = jpaRepository.findById(id)
+                .orElseThrow(()-> new CustomException(ErrorCode.AI_DIAGNOSIS_NOT_FOUND));
+
+        return aiReportDiseaseMapper.toDomain(entity);
     }
 }
--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/aiReportDisease/application/service/AiReportDiseaseService.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/aiReportDisease/application/service/AiReportDiseaseService.java
@@ -1,19 +1,28 @@
 package com.dog.health.dogbogamserver.domain.aiReportDisease.application.service;
 
 import com.dog.health.dogbogamserver.domain.aiReportDisease.application.port.in.CreateAiReportDiseaseUseCase;
+import com.dog.health.dogbogamserver.domain.aiReportDisease.application.port.in.FindAiReportDiseaseUseCase;
 import com.dog.health.dogbogamserver.domain.aiReportDisease.application.port.out.CreateAiReportDiseasePort;
+import com.dog.health.dogbogamserver.domain.aiReportDisease.application.port.out.FindAiReportDiseasePort;
 import com.dog.health.dogbogamserver.domain.aiReportDisease.application.service.dto.request.CreateAiReportDiseaseDto;
+import com.dog.health.dogbogamserver.domain.aiReportDisease.domain.AiReportDisease;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AiReportDiseaseService implements CreateAiReportDiseaseUseCase {
+public class AiReportDiseaseService implements CreateAiReportDiseaseUseCase, FindAiReportDiseaseUseCase {
 
     private final CreateAiReportDiseasePort createAiReportDiseasePort;
+    private final FindAiReportDiseasePort findAiReportDiseasePort;
 
     @Override
     public void createAiReportDisease(CreateAiReportDiseaseDto requestDto) {
         createAiReportDiseasePort.createAiReportDisease(requestDto);
+    }
+
+    @Override
+    public AiReportDisease findAiReportDiseaseById(Long id) {
+        return findAiReportDiseasePort.findAiReportDiseaseById(id);
     }
 }
