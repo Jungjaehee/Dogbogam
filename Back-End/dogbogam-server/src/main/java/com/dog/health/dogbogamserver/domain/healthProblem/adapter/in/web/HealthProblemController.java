63bd5ce6bd9bfebfa29e6570e7cdccd11a85dcb4
김수영 <kswim8914@gmail.com>
2024-09-30 07:58:35 +0900

    feat: 반려견의 건강고민들 조회하는 메소드 추가

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/healthProblem/adapter/in/web/HealthProblemController.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/healthProblem/adapter/in/web/HealthProblemController.java
@@ -1,9 +1,13 @@
 package com.dog.health.dogbogamserver.domain.healthProblem.adapter.in.web;
 
 import com.dog.health.dogbogamserver.domain.healthProblem.application.port.in.CreateHealthProblemUseCase;
+import com.dog.health.dogbogamserver.domain.healthProblem.application.port.in.FindHealthProblemsUseCase;
+import com.dog.health.dogbogamserver.domain.healthProblem.application.service.dto.response.HealthProblemResponse;
 import com.dog.health.dogbogamserver.global.web.dto.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/health-problems")
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class HealthProblemController {
 
     private final CreateHealthProblemUseCase createHealthProblemUseCase;
+    private final FindHealthProblemsUseCase findHealthProblemsUseCase;
 
     // 반려견 건강고민 등록
     @PostMapping
@@ -18,6 +23,15 @@ public class HealthProblemController {
         createHealthProblemUseCase.createHealthProblems(dogId, problem);
         return SuccessResponse.created();
     }
+
+    // 반려견의 건강고민들 조회
+    @GetMapping("/{dogId}")
+    public SuccessResponse<List<HealthProblemResponse>> findHealthProblems(@PathVariable Long dogId) {
+        List<HealthProblemResponse> healthProblems = findHealthProblemsUseCase.findHealthProblems(dogId);
+        return SuccessResponse.ok(healthProblems);
+    }
+
+
 
 
 }
