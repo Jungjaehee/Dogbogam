3fee5853980a2dbbfca7709a1dded7aabe6a9e42
Jungjaehee <wjfwogml5890@naver.com>
2024-09-29 18:40:23 +0900

    feat: 보험 추천을 위한 유스케이스, 포트 작성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurance/application/port/in/RecommandInsuranceUseCase.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurance/application/port/in/RecommandInsuranceUseCase.java
@@ -1,2 +1,8 @@
-package com.dog.health.dogbogamserver.domain.insurance.application.port.in;public class RecommandInsuranceUseCase {
+package com.dog.health.dogbogamserver.domain.insurance.application.port.in;
+
+import com.dog.health.dogbogamserver.domain.insurance.adapter.in.dto.DiagnosisItem;
+import com.dog.health.dogbogamserver.domain.insurance.adapter.in.dto.RecommandInsuranceResponseDto;
+
+public interface RecommandInsuranceUseCase {
+    RecommandInsuranceResponseDto recommandInsurance(DiagnosisItem diagnosisItem);
 }
--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurance/application/port/out/RecommandInsurancePort.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/insurance/application/port/out/RecommandInsurancePort.java
@@ -1,2 +1,11 @@
-package com.dog.health.dogbogamserver.domain.insurance.application.port.out;public interface RecommandInsurancePort {
+package com.dog.health.dogbogamserver.domain.insurance.application.port.out;
+
+import com.dog.health.dogbogamserver.domain.insurance.domain.InsuranceBenefit;
+
+import java.util.List;
+
+public interface RecommandInsurancePort {
+
+    List<InsuranceBenefit> findByBenefitAndAscFee(String benefit);
+
 }
