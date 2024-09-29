8ddd393cfc297fafc17dafed56b9435eb297705d
김수영 <kswim8914@gmail.com>
2024-09-30 08:10:50 +0900

    feat: 건강고민 삭제 UseCase, Port 추가

---
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/healthProblem/application/port/out/DeleteHealthProblemPort.java
@@ -0,0 +1,5 @@
+package com.dog.health.dogbogamserver.domain.healthProblem.application.port.out;
+
+public interface DeleteHealthProblemPort {
+    void deleteHealthProblem(Long healthProblemId);
+}
