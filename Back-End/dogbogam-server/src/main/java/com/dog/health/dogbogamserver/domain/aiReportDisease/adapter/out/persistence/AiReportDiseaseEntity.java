347afc144e68fd0b9c243a71f4fafb0490a78360
장현준 <50596733+gapple95@users.noreply.github.com>
2024-09-29 17:05:06 +0900

    feat: AiReportDisease  도메인, 엔터티 객체 생성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/aiReportDisease/adapter/out/persistence/AiReportDiseaseEntity.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/aiReportDisease/adapter/out/persistence/AiReportDiseaseEntity.java
@@ -1,4 +1,36 @@
 package com.dog.health.dogbogamserver.domain.aiReportDisease.adapter.out.persistence;
 
+import com.dog.health.dogbogamserver.domain.aiDiagnosis.adapter.out.persistence.AiDiagnosisEntity;
+import com.dog.health.dogbogamserver.domain.aiDiagnosis.domain.AiDiagnosis;
+import com.dog.health.dogbogamserver.domain.aiReportDisease.domain.AiReportDisease;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ai_report_diseases")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
 public class AiReportDiseaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ai_report_disease_id")
+    private Long aiReportDiseaseId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ai_diagnosis_id", nullable = false)
+    private AiDiagnosisEntity aiDiagnosis;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "percentage", nullable = false)
+    private String percentage;
+
+    @Column(name = "diagnosis_item", nullable = false)
+    private String diagnosisItem;
 }
--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/aiReportDisease/domain/AiReportDisease.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/aiReportDisease/domain/AiReportDisease.java
@@ -1,4 +1,16 @@
 package com.dog.health.dogbogamserver.domain.aiReportDisease.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
 public class AiReportDisease {
+    private Long aiReportDiseaseId;
+    private AiReportDisease aiReportDisease;
+    private String name;
+    private String percentage;
+    private String diagnosisItem;
 }
