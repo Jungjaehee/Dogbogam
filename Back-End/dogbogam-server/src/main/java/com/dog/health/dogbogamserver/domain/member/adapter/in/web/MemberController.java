9f27ae537f92be9022293d2fda0c6afe1727d6d0
김수영 <kswim8914@gmail.com>
2024-09-30 04:02:18 +0900

    feat: 이메일 중복체크 메소드 추가

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/member/adapter/in/web/MemberController.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/member/adapter/in/web/MemberController.java
@@ -1,11 +1,15 @@
 package com.dog.health.dogbogamserver.domain.member.adapter.in.web;
 
+import com.dog.health.dogbogamserver.domain.member.application.port.in.CheckMemberUseCase;
 import com.dog.health.dogbogamserver.domain.member.application.port.in.LoginUseCase;
 import com.dog.health.dogbogamserver.domain.member.application.port.in.RegisterMemberUseCase;
+import com.dog.health.dogbogamserver.domain.member.application.service.dto.request.CheckRequest;
 import com.dog.health.dogbogamserver.domain.member.application.service.dto.request.CreateRequest;
 import com.dog.health.dogbogamserver.domain.member.application.service.dto.request.LoginRequest;
+import com.dog.health.dogbogamserver.domain.member.application.service.dto.response.CheckResponse;
 import com.dog.health.dogbogamserver.domain.member.application.service.dto.response.LoginResponse;
 import com.dog.health.dogbogamserver.global.web.dto.response.SuccessResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +23,7 @@ public class MemberController {
 
     private final RegisterMemberUseCase registerMemberUseCase;
     private final LoginUseCase loginUseCase;
+    private final CheckMemberUseCase checkMemberUseCase;
 
     // 회원가입
     @PostMapping()
@@ -32,5 +37,14 @@ public class MemberController {
     public SuccessResponse<LoginResponse> login(@RequestBody LoginRequest loginRequest){
         LoginResponse loginResponse = loginUseCase.login(loginRequest);
         return SuccessResponse.ok(loginResponse);
+    }
+
+
+
+    // 이메일 중복 체크
+    @PostMapping("/check")
+    public SuccessResponse<?> checkDuplicateEmail(@RequestBody CheckRequest checkRequest) throws JsonProcessingException {
+        CheckResponse checkResponse = checkMemberUseCase.checkDuplicateEmail(checkRequest);
+        return SuccessResponse.ok(checkResponse);
     }
 }
