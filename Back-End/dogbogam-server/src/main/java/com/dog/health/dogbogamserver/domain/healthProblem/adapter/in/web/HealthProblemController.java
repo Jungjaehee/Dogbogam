a7cf2478380f96a25e18dea494ace656b439d59c
김수영 <kswim8914@gmail.com>
2024-09-30 08:16:34 +0900

    feat: request DTO 사용하도록 변경

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/healthProblem/adapter/in/web/HealthProblemController.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/healthProblem/adapter/in/web/HealthProblemController.java
@@ -3,9 +3,13 @@ package com.dog.health.dogbogamserver.domain.healthProblem.adapter.in.web;
 import com.dog.health.dogbogamserver.domain.healthProblem.application.port.in.CreateHealthProblemUseCase;
 import com.dog.health.dogbogamserver.domain.healthProblem.application.port.in.DeleteHealthProblemUseCase;
 import com.dog.health.dogbogamserver.domain.healthProblem.application.port.in.FindHealthProblemsUseCase;
+import com.dog.health.dogbogamserver.domain.healthProblem.application.service.dto.request.HealthProblemRequest;
 import com.dog.health.dogbogamserver.domain.healthProblem.application.service.dto.response.HealthProblemResponse;
+import com.dog.health.dogbogamserver.global.auth.dto.MemberPrincipal;
 import com.dog.health.dogbogamserver.global.web.dto.response.SuccessResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,21 +25,21 @@ public class HealthProblemController {
 
     // 반려견 건강고민 등록
     @PostMapping
-    public SuccessResponse<?> createHealthProblem(@RequestParam Long dogId, @RequestParam String problem) {
-        createHealthProblemUseCase.createHealthProblems(dogId, problem);
+    public SuccessResponse<?> createHealthProblem(@AuthenticationPrincipal MemberPrincipal memberPrincipal, @Valid @RequestBody HealthProblemRequest request) {
+        createHealthProblemUseCase.createHealthProblems(request.getDogId(), request.getProblem());
         return SuccessResponse.created();
     }
 
     // 반려견의 건강고민들 조회
     @GetMapping("/{dogId}")
-    public SuccessResponse<List<HealthProblemResponse>> findHealthProblems(@PathVariable Long dogId) {
+    public SuccessResponse<List<HealthProblemResponse>> findHealthProblems(@AuthenticationPrincipal MemberPrincipal memberPrincipal, @PathVariable Long dogId) {
         List<HealthProblemResponse> healthProblems = findHealthProblemsUseCase.findHealthProblems(dogId);
         return SuccessResponse.ok(healthProblems);
     }
 
     // healthProblem 삭제
     @DeleteMapping("/{healthProblemId}")
-    public SuccessResponse<?> deleteHealthProblem(@PathVariable Long healthProblemId) {
+    public SuccessResponse<?> deleteHealthProblem(@AuthenticationPrincipal MemberPrincipal memberPrincipal, @PathVariable Long healthProblemId) {
         deleteHealthProblemUseCase.deleteHealthProblem(healthProblemId);
         return SuccessResponse.ok();
     }
