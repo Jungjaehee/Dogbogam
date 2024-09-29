71a79b2e92c793d72d1a451d06ed3b6b3a17c934
김수영 <kswim8914@gmail.com>
2024-09-30 07:41:35 +0900

    feat: 건강고민 mapper toDomain, toEntity 생성

---
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/healthProblem/adapter/out/persistence/HealthProblemMapper.java
@@ -0,0 +1,27 @@
+package com.dog.health.dogbogamserver.domain.healthProblem.adapter.out.persistence;
+
+import com.dog.health.dogbogamserver.domain.dog.adapter.out.persistence.DogMapper;
+import com.dog.health.dogbogamserver.domain.healthProblem.domain.HealthProblem;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class HealthProblemMapper {
+
+    private final DogMapper dogMapper;
+
+    public HealthProblemEntity toEntity(HealthProblem healthProblem) {
+        return HealthProblemEntity.builder()
+                .dog(dogMapper.toEntity(healthProblem.getDog()))  // Dog 변환
+                .problem(healthProblem.getProblem())
+                .build();
+    }
+
+    public HealthProblem toDomain(HealthProblemEntity entity) {
+        return HealthProblem.builder()
+                .dog(dogMapper.toDomain(entity.getDog()))  // Dog 변환
+                .problem(entity.getProblem())
+                .build();
+    }
+}
