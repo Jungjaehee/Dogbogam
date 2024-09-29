c446d49c737a0b743e4b825a6213f6a0736c2f28
장현준 <50596733+gapple95@users.noreply.github.com>
2024-09-29 19:20:06 +0900

    feat: Service 작성

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/aiDiagnosis/application/service/AiDiagnosisService.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/aiDiagnosis/application/service/AiDiagnosisService.java
@@ -1,4 +1,19 @@
 package com.dog.health.dogbogamserver.domain.aiDiagnosis.application.service;
 
-public class AiDiagnosisService {
+import com.dog.health.dogbogamserver.domain.aiDiagnosis.application.port.in.CreateAiDiagnosisUseCase;
+import com.dog.health.dogbogamserver.domain.aiDiagnosis.application.port.out.CreateAiDiagnosisPort;
+import com.dog.health.dogbogamserver.domain.aiDiagnosis.application.service.dto.request.CreateAiDiagnosisRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AiDiagnosisService implements CreateAiDiagnosisUseCase {
+
+    private static CreateAiDiagnosisPort createAiDiagnosisPort;
+
+    @Override
+    public void createAiDiagnosis(CreateAiDiagnosisRequestDto requestDto) {
+        createAiDiagnosisPort.createAiDiagnosis(requestDto);
+    }
 }
