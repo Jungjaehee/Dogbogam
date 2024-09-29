e13a92ef01c68565263fe369a90f6ec0af08b0ef
장현준 <50596733+gapple95@users.noreply.github.com>
2024-09-30 03:52:21 +0900

    chore: CustomException 주석 및 정리

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/global/web/exception/ErrorCode.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/global/web/exception/ErrorCode.java
@@ -5,24 +5,24 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
+    // 멤버
     USER_NOT_FOUND("존재하지 않는 유저입니다.", HttpStatus.UNAUTHORIZED),
+    USER_VALIDATION_ERROR("유저 검증 오류가 발생했습니다.", HttpStatus.UNAUTHORIZED),
     INVALID_PASSWORD("올바르지 않은 비밀번호입니다.", HttpStatus.UNAUTHORIZED),
     INVALID_ACCESS_TOKEN("올바르지 않은 ACCESSTOKEN 입니다", HttpStatus.UNAUTHORIZED),
     JSON_PROCESSING_ERROR("JSON 처리 중 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-
+    // 반려견
     DOG_NOT_FOUND("존재하지 않는 반려견입니다.", HttpStatus.NOT_FOUND),
     DOG_LIST_NOT_FOUND("해당 유저는 반려견이 없습니다.", HttpStatus.NOT_FOUND),
-    USER_VALIDATION_ERROR("유저 검증 오류가 발생했습니다.", HttpStatus.UNAUTHORIZED),
-
     DOG_NO_ACCESS("해당 반려견에 대한 접근 권한이 존재하지 않습니다.", HttpStatus.UNAUTHORIZED),
+    // 보험
     INSURANCE_NOT_FOUND("존재하지 않는 보험입니다.", HttpStatus.NOT_FOUND),
     INSURANCE_RECORD_NOT_FOUND("존재하지 않는 보험 기록입니다.",HttpStatus.FOUND),
     INSURANCE_RECORD_ALREADY_EXIST("이미 등록된 보험입니다.", HttpStatus.CONFLICT),
     INSURANCE_RECORD_NO_ACCESS("해당 보험 기록에 대한 접근 권한이 존재하지 않습니다.", HttpStatus.UNAUTHORIZED),
-
     INSURANCE_RECOMMEND_NOT_FOUND("추천 보험을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     INSURANCE_BENEFIT_NOT_FOUND("존재하지 않는 보장 혜택입니다.", HttpStatus.NOT_FOUND),
-
+    // AI 진단
     AI_DIAGNOSIS_NOT_FOUND("해당 AI 예측 기록이 없습니다.", HttpStatus.NOT_FOUND);
 
     private final String message;
