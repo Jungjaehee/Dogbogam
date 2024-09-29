0188c801f40bdeed640aa8a7bc1464cc9cf5174c
김수영 <kswim8914@gmail.com>
2024-09-30 07:46:44 +0900

    feat: Adapter 생성, 건강고민 등록에 필요한 메소드들 추가

---
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/healthProblem/adapter/out/persistence/HealthProblemPersistenceAdapter.java
@@ -0,0 +1,39 @@
+package com.dog.health.dogbogamserver.domain.healthProblem.adapter.out.persistence;
+
+import com.dog.health.dogbogamserver.domain.healthProblem.application.port.out.LoadHealthProblemPort;
+import com.dog.health.dogbogamserver.domain.healthProblem.application.port.out.SaveHealthProblemPort;
+import com.dog.health.dogbogamserver.domain.healthProblem.domain.HealthProblem;
+import com.dog.health.dogbogamserver.domain.dog.application.port.out. FindDogDetailsPort;
+import com.dog.health.dogbogamserver.domain.dog.domain.Dog;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class HealthProblemPersistenceAdapter implements LoadHealthProblemPort, SaveHealthProblemPort {
+
+    private final HealthProblemSpringDataRepository repository;
+    private final HealthProblemMapper mapper;
+    private final  FindDogDetailsPort findDogDetailsPort;
+
+    @Override
+    public List<HealthProblem> loadHealthProblemsByDogId(Long dogId) {
+        return repository.findByDog_DogId(dogId).stream()
+                .map(mapper::toDomain)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void saveHealthProblem(HealthProblem healthProblem) {
+        repository.save(mapper.toEntity(healthProblem));
+    }
+
+    @Override
+    public Optional<Dog> loadDogById(Long dogId) {
+        return findDogDetailsPort.findByDogId(dogId);
+    }
+}
