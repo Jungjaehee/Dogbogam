fd2c2f27e71e2d6da03c4ede298fce4208378ca2
장현준 <50596733+gapple95@users.noreply.github.com>
2024-09-30 12:45:43 +0900

    fix: ResponseDto @Getter 수정

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/aiDiagnosis/application/service/dto/response/CreateAiDiagnosisResponseDto.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/aiDiagnosis/application/service/dto/response/CreateAiDiagnosisResponseDto.java
@@ -3,11 +3,13 @@ package com.dog.health.dogbogamserver.domain.aiDiagnosis.application.service.dto
 import com.dog.health.dogbogamserver.domain.aiDiagnosis.domain.AiDiagnosis;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
 
 @Builder
 @AllArgsConstructor
+@Getter
 public class CreateAiDiagnosisResponseDto {
     private int size;
     private long totalElements;
