1c1a95d6a4d022ccee0e23f7899f3db60f2cf56e
김수영 <kswim8914@gmail.com>
2024-09-30 14:00:55 +0900

    fix: 회원가입 시 빈 jwt 토큰이 만들어지는 오류 수정

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/member/application/port/out/SaveMemberPort.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/member/application/port/out/SaveMemberPort.java
@@ -3,5 +3,5 @@ package com.dog.health.dogbogamserver.domain.member.application.port.out;
 import com.dog.health.dogbogamserver.domain.member.domain.Member;
 
 public interface SaveMemberPort {
-    void saveMember(Member member);
+    Long saveMember(Member member);
 }
