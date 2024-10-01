ca86780a00e0fe947e77a2ecdf1f91fcec31851e
wkdgu <gapple95@naver.com>
2024-10-01 23:14:56 +0900

    fix: 반려견 리스트 조회 UseCase 수정

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/dog/application/port/in/FindDogsUseCase.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/dog/application/port/in/FindDogsUseCase.java
@@ -1,10 +1,11 @@
 package com.dog.health.dogbogamserver.domain.dog.application.port.in;
 
+import com.dog.health.dogbogamserver.domain.dog.application.service.dto.responseDto.FindDogsResponseDto;
 import com.dog.health.dogbogamserver.domain.dog.domain.Dog;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface FindDogsUseCase {
-    Optional<List<Dog>> findDogsByMemberId(Long memberId);
+    Optional<FindDogsResponseDto> findDogsByMemberId(Long memberId, int page, int size);
 }
