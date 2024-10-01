086a8db06cfca774b255c3448d5e76280432fbe6
wkdgu <gapple95@naver.com>
2024-10-01 23:14:03 +0900

    fix: Dto 작성

---
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/dog/application/service/dto/responseDto/FindDogsResponseDto.java
@@ -0,0 +1,27 @@
+package com.dog.health.dogbogamserver.domain.dog.application.service.dto.responseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class FindDogsResponseDto {
+    int size;
+    Long totalElements;
+    Long currentPage;
+    Long totalPages;
+    List<DogDto> dogList;
+}
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class DogDto {
+    Long dogId;
+    String dogName;
+    String dogUrl;
+}
