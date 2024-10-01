60c1f30733f9954ab0a8a96418f8bb2a7ea04258
김수영 <kswim8914@gmail.com>
2024-10-01 15:44:25 +0900

    fix: 건강 고민 ENUM 타입 수정

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/healthProblem/domain/ProblemType.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/healthProblem/domain/ProblemType.java
@@ -1,7 +1,7 @@
 package com.dog.health.dogbogamserver.domain.healthProblem.domain;
 
 public enum ProblemType {
-    눈건강, 피모관리, 면역력강화, 식욕증진, 구강관리, 스트레스완화, 분리불안해소, 체중유지, 소화개선, 심장건강, 관절강화;
+    눈, 관절, 면역력강화, 피모, 구강, 비만, 심장, 변비, 스트레스;
 
     public static boolean isValid(String problem) {
         for (ProblemType p : values()) {
