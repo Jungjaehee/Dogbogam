934f941d0ee8952eabf5787196274a2530471f35
장현준 <50596733+gapple95@users.noreply.github.com>
2024-10-02 15:54:27 +0900

    fix: Controller @ModelAttribute 추가

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/vaccinationRecords/adapter/in/web/VaccinationRecordController.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/vaccinationRecords/adapter/in/web/VaccinationRecordController.java
@@ -5,6 +5,7 @@ import com.dog.health.dogbogamserver.domain.vaccinationRecords.application.servi
 import com.dog.health.dogbogamserver.domain.vaccinationRecords.application.service.dto.request.UpdateVaccinationRecordRequestDto;
 import com.dog.health.dogbogamserver.domain.vaccinationRecords.domain.VaccinationRecord;
 import com.dog.health.dogbogamserver.global.web.dto.response.SuccessResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -28,44 +29,28 @@ public class VaccinationRecordController {
     private final FindVaccinationRecordsUseCase findVaccinationRecordsUseCase;
 
     @Operation(summary = "특정 접종 기록 조회", description = "ID로 특정 백신 접종 기록을 조회합니다.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "성공적으로 접종 기록을 반환했습니다."),
-            @ApiResponse(responseCode = "404", description = "접종 기록을 찾을 수 없습니다.")
-    })
     @GetMapping("/{recordId}")
     public SuccessResponse<VaccinationRecord> getVaccinationRecordById(@PathVariable("recordId") Long recordId) {
         return SuccessResponse.ok(findVaccinationRecordUseCase.findVaccinationRecordById(recordId));
     }
 
     @Operation(summary = "접종 기록 생성", description = "새로운 백신 접종 기록을 생성합니다.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "201", description = "접종 기록이 성공적으로 생성되었습니다."),
-            @ApiResponse(responseCode = "400", description = "잘못된 요청입니다.")
-    })
     @PostMapping
-    public SuccessResponse<?> createVaccinationRecord(@RequestBody CreateVaccinationRecordRequestDto createVaccinationRecordRequestDto) {
+    public SuccessResponse<?> createVaccinationRecord(@ModelAttribute @Valid CreateVaccinationRecordRequestDto createVaccinationRecordRequestDto) {
         log.info("Controller Create Vaccination record: {}", createVaccinationRecordRequestDto);
         createVaccinationRecordUseCase.createVaccinationRecord(createVaccinationRecordRequestDto);
         return SuccessResponse.created();
     }
 
     @Operation(summary = "접종 기록 업데이트", description = "기존 백신 접종 기록을 업데이트합니다.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "접종 기록이 성공적으로 업데이트되었습니다."),
-            @ApiResponse(responseCode = "404", description = "접종 기록을 찾을 수 없습니다.")
-    })
     @PatchMapping
-    public SuccessResponse<?> updateVaccinationRecord(@RequestBody UpdateVaccinationRecordRequestDto updateVaccinationRecordRequestDto) {
+    public SuccessResponse<?> updateVaccinationRecord(@ModelAttribute @Valid UpdateVaccinationRecordRequestDto updateVaccinationRecordRequestDto) {
         log.info("Controller Update Vaccination record: {}", updateVaccinationRecordRequestDto);
         updateVaccinationRecordUseCase.updateVaccinationRecord(updateVaccinationRecordRequestDto);
         return SuccessResponse.updated();
     }
 
     @Operation(summary = "접종 기록 삭제", description = "특정 백신 접종 기록을 삭제합니다.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "접종 기록이 성공적으로 삭제되었습니다."),
-            @ApiResponse(responseCode = "404", description = "접종 기록을 찾을 수 없습니다.")
-    })
     @DeleteMapping("/{recordId}")
     public SuccessResponse<?> deleteVaccinationRecord(@PathVariable("recordId") Long recordId) {
         log.info("Controller Delete Vaccination record: {}", recordId);
@@ -74,10 +59,6 @@ public class VaccinationRecordController {
     }
 
     @Operation(summary = "반려견 접종 기록 조회", description = "특정 반려견의 모든 백신 접종 기록을 조회합니다.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "접종 기록 목록을 성공적으로 반환했습니다."),
-            @ApiResponse(responseCode = "404", description = "접종 기록을 찾을 수 없습니다.")
-    })
     @GetMapping("/dog/{dogId}")
     public SuccessResponse<List<VaccinationRecord>> getAllVaccinationRecordsForDog(@PathVariable("dogId") Long dogId) {
         return SuccessResponse.ok(findVaccinationRecordsUseCase.findVaccinationsByDogId(dogId));
