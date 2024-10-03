08d6081f81c2aa07a0eef730180c2a10e9817d85
정재희 <wjfwogml5890@naver.com>
2024-10-03 21:56:54 +0900

    feat: 서비스 작성

---
+++ AI/service/service.py
@@ -0,0 +1,55 @@
+from fastapi import HTTPException
+from ultralytics import YOLO
+from mapper.eye_map import EyeLabelMapper
+import os
+import logging
+import traceback
+
+async def diagnosis_eye(image):
+    model_path = "../model/eye/yolov8.pt"
+    outputs = await ai_inference(model_path, image, EyeLabelMapper)
+    return outputs
+
+
+async def diagnosis_skin(image):
+    return
+
+
+async def diagnosis_obesity(image):
+    return
+
+async def diagnosis_breed(image):
+    return
+
+
+async def ai_inference(model_path, image, label_mapper):
+    try:
+        os.makedirs("../data", exist_ok=True)
+        image_path = f"./data/{image.filename}"
+        contents = await image.read()
+        with open(image_path, "wb") as f:
+            f.write(contents)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="이미지 처리가 되지 않았습니다.")
+
+    outputs = []
+    try:
+        model = YOLO(model_path, task='classify').cuda()
+        result = model.predict(image_path, imgsz=224)
+
+        for cls_idx, conf in zip(result[0].probs.top5[:3], result[0].probs.top5conf[:3]):
+            outputs.append({
+                "label": label_mapper.get_label(cls_idx),
+                "conf": round(conf.item() * 100, 2)
+            })
+        logging.log(1, outputs)
+        os.remove(image_path)
+
+    except Exception as e:
+        os.remove(image_path)
+        logging.error("Exception occurred", exc_info=True)  # 로그에 예외 정보를 출력
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail="모델 예측이 불가합니다.")
+    
+    return outputs
