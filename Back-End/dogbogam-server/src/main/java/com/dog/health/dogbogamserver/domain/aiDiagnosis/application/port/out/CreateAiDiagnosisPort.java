770efaa2f1510c5aa381b6b2f791f61c8c4eb93b
정재희 <wjfwogml5890@naver.com>
2024-10-03 21:29:17 +0900

    feat: AI 진단 결과 저장 후 리포트 아이디를 얻기 위해 반환 타입 수정

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/aiDiagnosis/application/port/out/CreateAiDiagnosisPort.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/aiDiagnosis/application/port/out/CreateAiDiagnosisPort.java
@@ -3,5 +3,5 @@ package com.dog.health.dogbogamserver.domain.aiDiagnosis.application.port.out;
 import com.dog.health.dogbogamserver.domain.aiDiagnosis.application.service.dto.request.CreateAiDiagnosisRequestDto;
 
 public interface CreateAiDiagnosisPort {
-    void createAiDiagnosis(Long memberId, CreateAiDiagnosisRequestDto requestDto);
+    Long createAiDiagnosis(Long memberId, CreateAiDiagnosisRequestDto requestDto);
 }
