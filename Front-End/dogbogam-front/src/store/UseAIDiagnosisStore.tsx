019e3ce927cd16f24356f7772c70376349163b5c
김준혁 <junhuk4079@gmail.com>
2024-10-04 16:09:37 +0900

    [FEAT] AI 진단기록 스토어 추가

---
+++ Front-End/dogbogam-front/src/store/UseAIDiagnosisStore.tsx
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import type { AiDiagnosis, Disease } from "../models/record.model";
+
+interface StoreState {
+  diagnosisList: AiDiagnosis[]; // AI 진단 리스트
+  setDiagnosisList: (newList: AiDiagnosis[]) => void; // AI 진단 리스트 전체
+  diagnosisDetail: AiDiagnosis; // 현재 진단 상세정보
+  setDiagnosis: (newInfo: Partial<AiDiagnosis>) => void; // 현재 진단 상세정보
+}
+
+const useAIDiagnosisStore = create(
+  persist<StoreState>(
+    (set) => ({
+      diagnosisList: [],
+      setDiagnosisList: (newList: AiDiagnosis[]) =>
+        set(() => ({
+          diagnosisList: newList, // 배열 전체를 업데이트
+        })),
+      diagnosisDetail: {
+        reportId: 0,
+        dogId: 0,
+        createdAt: "" as unknown as Date, // 만약 Date 객체로 관리하려면 new Date()로 설정 가능
+        imageName: "",
+        imageUrl: "",
+        normal: false,
+        diagnosisItem: "",
+        diseases: [] as Disease[], // diseases 배열 초기화
+      },
+      setDiagnosis: (newInfo: Partial<AiDiagnosis>) =>
+        set((state) => ({
+          diagnosisDetail: { ...state.diagnosisDetail, ...newInfo },
+        })),
+    }),
+    {
+      name: "diagnosisStorage", // 저장소 이름
+    }
+  )
+);
+
+export default useAIDiagnosisStore;
