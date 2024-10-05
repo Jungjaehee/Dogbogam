b5f66a3a1d0fc9556d51cdcac8cd821444992084
김수영 <kswim8914@gmail.com>
2024-10-05 14:17:08 +0900

    feat: 영양제 상세조회 메소드 추가

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/supplement/application/service/SupplementService.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/supplement/application/service/SupplementService.java
@@ -1,9 +1,12 @@
 package com.dog.health.dogbogamserver.domain.supplement.application.service;
 
-import com.dog.health.dogbogamserver.domain.supplement.application.port.in.FindSupplementsUseCase;
+import com.dog.health.dogbogamserver.domain.supplement.application.port.in.FindSupplementUseCase;
 import com.dog.health.dogbogamserver.domain.supplement.application.port.out.LoadSupplementPort;
+import com.dog.health.dogbogamserver.domain.supplement.application.service.dto.response.SupplementDetailResponse;
 import com.dog.health.dogbogamserver.domain.supplement.application.service.dto.response.SupplementSummaryResponse;
 import com.dog.health.dogbogamserver.domain.supplement.domain.Supplement;
+import com.dog.health.dogbogamserver.global.web.exception.CustomException;
+import com.dog.health.dogbogamserver.global.web.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +15,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class SupplementService implements FindSupplementsUseCase {
+public class SupplementService implements FindSupplementUseCase {
 
     private final LoadSupplementPort loadSupplementPort;
 
@@ -28,4 +31,28 @@ public class SupplementService implements FindSupplementsUseCase {
                         .build())
                 .collect(Collectors.toList());
     }
+
+    @Override
+    public SupplementDetailResponse findSupplementById(Long supplementId) {
+        Supplement supplement = loadSupplementPort.findSupplementById(supplementId)
+                .orElseThrow(() -> new CustomException(ErrorCode.SUPPLEMENT_NOT_FOUND));
+
+        return SupplementDetailResponse.builder()
+                .supplementId(supplement.getSupplementId())
+                .productName(supplement.getProductName())
+                .brandName(supplement.getBrandName())
+                .target(supplement.getTarget())
+                .how(supplement.getHow())
+                .offer(supplement.getOffer())
+                .type(supplement.getType())
+                .basis(supplement.getBasis())
+                .protein(supplement.getProtein())
+                .fat(supplement.getFat())
+                .feature(supplement.getFeature())
+                .price(supplement.getPrice())
+                .imageName(supplement.getImageName())
+                .imageUrl(supplement.getImageUrl())
+                .build();
+    }
+
 }
