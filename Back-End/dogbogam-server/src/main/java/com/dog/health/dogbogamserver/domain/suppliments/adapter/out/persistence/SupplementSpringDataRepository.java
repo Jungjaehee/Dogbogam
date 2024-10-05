c6bea2e2cb14a3a0fb2c95d7c60a12815d7e3242
김수영 <kswim8914@gmail.com>
2024-10-05 12:57:11 +0900

    feat: SupplementSpringDataRepository 구현

---
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/suppliments/adapter/out/persistence/SupplementSpringDataRepository.java
@@ -0,0 +1,6 @@
+package com.dog.health.dogbogamserver.domain.suppliments.adapter.out.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplementSpringDataRepository extends JpaRepository<SupplementEntity, Long> {
+}
