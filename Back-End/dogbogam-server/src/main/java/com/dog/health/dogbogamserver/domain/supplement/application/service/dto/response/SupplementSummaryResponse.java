bc397c5eceefb262b571acaf441f5701701ad941
김수영 <kswim8914@gmail.com>
2024-10-06 18:30:14 +0900

    fix: 모든 영양제 조회 response dto 에서 builder 사용하도록 수정

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/supplement/application/service/dto/response/SupplementSummaryResponse.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/supplement/application/service/dto/response/SupplementSummaryResponse.java
@@ -1,17 +1,31 @@
 package com.dog.health.dogbogamserver.domain.supplement.application.service.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class SupplementSummaryResponse {
+
     private Long supplementId;
     private String productName;
     private String imageUrl;
     private Integer price;
+
+    @Builder
+    public SupplementSummaryResponse(Long supplementId, String productName, String imageUrl, Integer price) {
+        this.supplementId = supplementId;
+        this.productName = productName;
+        this.imageUrl = imageUrl;
+        this.price = price;
+    }
+
+    public static SupplementSummaryResponse from(Long supplementId, String productName, String imageUrl, Integer price) {
+        return SupplementSummaryResponse.builder()
+                .supplementId(supplementId)
+                .productName(productName)
+                .imageUrl(imageUrl)
+                .price(price)
+                .build();
+    }
 }
+
