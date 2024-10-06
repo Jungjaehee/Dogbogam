5dc04582b631cef0d83408445f3f6052de87c972
김수영 <kswim8914@gmail.com>
2024-10-06 22:19:47 +0900

    fix: 모든 영양제 조회 dto 수정

--- Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/supplement/application/service/dto/response/SupplementSummaryResponse.java
+++ Back-End/dogbogam-server/src/main/java/com/dog/health/dogbogamserver/domain/supplement/application/service/dto/response/SupplementSummaryResponse.java
@@ -9,21 +9,24 @@ public class SupplementSummaryResponse {
     private Long supplementId;
     private String productName;
     private String imageUrl;
+    private String offer;
     private Integer price;
 
     @Builder
-    public SupplementSummaryResponse(Long supplementId, String productName, String imageUrl, Integer price) {
+    public SupplementSummaryResponse(Long supplementId, String productName, String imageUrl, String offer, Integer price) {
         this.supplementId = supplementId;
         this.productName = productName;
         this.imageUrl = imageUrl;
+        this.offer = offer;
         this.price = price;
     }
 
-    public static SupplementSummaryResponse from(Long supplementId, String productName, String imageUrl, Integer price) {
+    public static SupplementSummaryResponse from(Long supplementId, String productName, String imageUrl, String offer, Integer price) {
         return SupplementSummaryResponse.builder()
                 .supplementId(supplementId)
                 .productName(productName)
                 .imageUrl(imageUrl)
+                .offer(offer)
                 .price(price)
                 .build();
     }
