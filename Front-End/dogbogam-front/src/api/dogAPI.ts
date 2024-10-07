8f540113853598e636b371129a523eac5f6843f2
김준혁 <junhuk4079@gmail.com>
2024-10-07 11:16:13 +0900

    [FEAT] 강아지 정보 수정 API 추가

--- Front-End/dogbogam-front/src/api/dogAPI.ts
+++ Front-End/dogbogam-front/src/api/dogAPI.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "./APIconfig";
 import { inputDogInfo } from "../models/dog.model";
 import defaultImage from "../assets/MyPage/DefaultDogIcon.png";
+import axiosInstance from "./axiosinstance";
 
 export const registDogInfo = async (token: string, dog: inputDogInfo) => {
   try {
@@ -85,6 +86,48 @@ export const getDogInfo = async (token: string, dogId: number) => {
     return response?.data?.data;
   } catch (error) {
     console.log("강아지 정보 조회: ", error);
+    throw error;
+  }
+};
+
+export const UpdateHealth = async (dogId: number, problem: string[]) => {
+  try {
+    const response = await axiosInstance.post(`${BASE_URL}health-problems`,{dogId,problem});
+    return response?.data?.data;
+  } catch (error) {
+    console.log("건강 고민 수정 실패: ", error);
+    throw error;
+  }
+};
+
+export const updateDogInfo = async (dog: inputDogInfo) => {
+  try {
+    const formData = new FormData();
+
+    // 이미지가 있으면 첨부, 없으면 디폴트 이미지
+    if (dog.image) {
+      formData.append("image", dog.image);
+    } else {
+      const defaultDogImage = await fetch(defaultImage).then((res) =>
+        res.blob()
+      );
+      formData.append("image", defaultDogImage, "default-dog.png"); // 디폴트 이미지
+    }
+    formData.append("name", dog.name);
+    formData.append("gender", dog.gender);
+    formData.append("breed", dog.breed);
+    formData.append("birthDate", dog.birthDate);
+    formData.append("weight", dog.weight.toString());
+    formData.append("isNeutered", dog.isNeutered.toString());
+
+    const response = await axiosInstance.patch(`${BASE_URL}dogs`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response?.data?.data;
+  } catch (error) {
+    console.log("강아지 정보 수정 실패: ", error);
     throw error;
   }
 };
