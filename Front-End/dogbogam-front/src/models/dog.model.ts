22f457d88469e58f5b4518b0c6153d6cdaf30133
김준혁 <junhuk4079@gmail.com>
2024-10-07 12:46:20 +0900

    [FEAT] 정보 수정용 타입 생성

--- Front-End/dogbogam-front/src/models/dog.model.ts
+++ Front-End/dogbogam-front/src/models/dog.model.ts
@@ -27,3 +27,14 @@ export interface DogList {
   dogName: string;
   imageUrl: string;
 }
+
+export interface updateDogInfo {
+  image: File | null;
+  dogId: number;
+  name: string;
+  gender: string; // 여아 남아
+  breed: string;
+  birthDate: string; // 강아지 고양이
+  weight: number;
+  isNeutered: boolean;
+}
