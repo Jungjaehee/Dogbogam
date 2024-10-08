504522df0c5f2d680936665aaf12282b52a89154
김준혁 <junhuk4079@gmail.com>
2024-10-09 01:23:55 +0900

    [FEAT] AI 진단 결과 상세조회 API 연결 완료 및 완성

--- Front-End/dogbogam-front/src/pages/Mypage/AIDiagnosisDetail.tsx
+++ Front-End/dogbogam-front/src/pages/Mypage/AIDiagnosisDetail.tsx
@@ -1,5 +1,4 @@
-import { useLocation, useNavigate } from "react-router-dom";
-import { Button } from "../../components/Button";
+import { useLocation} from "react-router-dom";
 import { TopBar } from "../../components/Topbar";
 import { useEffect, useState } from "react";
 import { Normal } from "../../pages/AIprediction/components/Normal";
@@ -12,87 +11,80 @@ import { recommendInsurance } from "../../models/insurance.model";
 import { recommendSupplement } from "../../models/supplement.model";
 import useUserStore from "../../store/UseUserStore";
 import { getDiagnosisDetail } from "../../api/aiDiagnosisAPI";
+import { getAiDiagnosisDay } from "../../utils/calcDate";
 
 export const AIDiagnosisDetail = () => {
-  const navigate = useNavigate();
   const location = useLocation();
   const { id } = location.state;
   const { dogInfo } = useUserStore();
-  console.log(dogInfo)
 
-  const [result, setResult] = useState<AiDiagnosis>();
-  console.log(result)
-  const [insurance, setInsurance] = useState<recommendInsurance>({
-    insuranceId: 0, 
-    name: "",
-    fee: "",
-    company: "",
-    image: "",
-  });
-  const [nutrient, setNutrient] = useState<recommendSupplement>({
-    supplementId: 0,
-    productName: "",
-    offer: "",
-    price: 0,
-    imageUrl: "",
-  });
+   const [result, setResult] = useState<AiDiagnosis>({
+     aiDiagnosisId: id,
+     dogId: 0,
+     createdAt: new Date(),
+     imageUrl: "",
+     normal: true,
+     diagnosisItem: "",
+     diseases: [],
+   });
+   const [insurance, setInsurance] = useState<recommendInsurance>({
+     insuranceId: 0,
+     name: "",
+     fee: "",
+     company: "",
+     image: "",
+   });
+   const [nutrient, setNutrient] = useState<recommendSupplement>({
+     supplementId: 0,
+     productName: "",
+     offer: "",
+     price: 0,
+     imageUrl: "",
+   });  
 
-  const confirm = () => {
-    navigate("/home");
-  };
+   const getResult = async () => {
+     const resultResponse = await getDiagnosisDetail(id);
+     setResult(resultResponse);
+     const insuranceResponse = await getInsurance(resultResponse.diagnosisItem);
+     setInsurance(insuranceResponse);
+     const problemsArray = dogInfo.healthProblems.map(
+       (health) => health.problem
+     );
+     problemsArray.push(resultResponse.diagnosisItem);
+     const resultString = problemsArray.join(", ");
+     const nutrientResponse = await getNutrient(resultString);
+     setNutrient(nutrientResponse);
+   };
 
-  const getResult = async () => {
-    const resultResponse = await getDiagnosisDetail(id);
-    setResult(resultResponse);
-    console.log(resultResponse);
-    const insuranceResponse = await getInsurance(resultResponse.diagnosisItem);
-    setInsurance(insuranceResponse);
-    console.log(resultResponse);
-    const problemsArray = dogInfo.healthProblems.map(
-      (health) => health.problem
-    );
-    problemsArray.push(resultResponse.diagnosisItem);
-    const resultString = problemsArray.join(", ");
-    const nutrientResponse = await getNutrient(resultString);
-    setNutrient(nutrientResponse);
-  };
+   useEffect(() => {
+     getResult();
+   }, []);
 
-  useEffect(() => {
-    getResult();
-  }, []);
+const formattedDate = getAiDiagnosisDay(result.createdAt)
 
   return (
     <div className="h-full pt-6 px-4 bg-white flex flex-col justify-between overflow-y-auto">
       <div className="">
         <TopBar pre={""} title={""} skip={""} />
-        <div className="space-y-3 mb-10">
+        <div className="space-y-1 mb-1">
           <p className="text-xl font-semibold">
             <span className="text-main-color">{dogInfo.name}</span>의{" "}
-            {/* <span className="text-main-color">{result.diagnosisItem}</span> */}
+            <span className="text-main-color">{result.diagnosisItem}</span>
             <br />
             AI 질병 예측 리포트
           </p>
-          <p className="text-gray-400 font-medium text-sm">
-            {/* {result.createdAt.toLocaleDateString()} */}
-          </p>
+          <p className="text-gray-400 font-medium text-sm">{formattedDate}요일에 실시한 검사 결과에요</p>
         </div>
-        <div className="space-y-10 mb-10">
-          {/* {result.normal ? ( */}
-            {/* <Normal result={result} /> */}
-          {/* ) : ( */}
-            {/* <Abnormal result={result} /> */}
-          {/* )} */}
+        <div className="space-y-8 mb-1">
+          {result.normal ? (
+            <Normal result={result} />
+          ) : (
+            <Abnormal result={result} />
+          )}
           <RecommendNutrient nutrient={nutrient} />
           <RecommendInsurance insurance={insurance} />
         </div>
       </div>
-      <Button
-        text={"확인 완료"}
-        onClick={() => {
-          confirm();
-        }}
-        bgColor={"bg-main-color"}
-      />
     </div>
   );
 };
