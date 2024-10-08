6fee621d9c95da01faa1f57ceb70f1e1756d411e
김준혁 <junhuk4079@gmail.com>
2024-10-08 16:01:54 +0900

    [CHORE] 주석 수정

--- Front-End/dogbogam-front/src/api/aiDiagnosisAPI.ts
+++ Front-End/dogbogam-front/src/api/aiDiagnosisAPI.ts
@@ -1,7 +1,7 @@
 // import type { AiDiagnosis } from "../models/record.model";
 import axiosInstance from "./axiosinstance";
 
-// 진료 기록 삭제 API 요청 함수
+// ai 진단 삭제 API 요청 함수
 export const deleteDiagnosis = async (diagnosisId: number) => {
   try {
     const response = await axiosInstance.delete(
@@ -15,7 +15,7 @@ export const deleteDiagnosis = async (diagnosisId: number) => {
   }
 };
 
-// 진료 기록 디테일 조회 API 요청 함수
+// ai 진단 디테일 조회 API 요청 함수
 export const getDiagnosisDetail = async (diagnosisId: number) => {
   try {
     const response = await axiosInstance.get(
@@ -29,11 +29,11 @@ export const getDiagnosisDetail = async (diagnosisId: number) => {
   }
 };
 
-// 진료 목록 조회 API 요청 함수
+// ai 진단 목록 조회 API 요청 함수
 export const getDiagnosisRecords = async (dogId: number) => {
   try {
     const response = await axiosInstance.get(`/ai-diagnosis/report/list/${dogId}`);
-    return response.data;
+    return response.data.data;
   } catch (error) {
     console.error("ai 진단 목록 불러오기 실패 :", error);
   }
