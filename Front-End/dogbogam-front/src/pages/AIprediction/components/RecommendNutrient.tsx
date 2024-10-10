cf8cd801884303291e2ab6a9ca487b50f0b30eff
김준혁 <junhuk4079@gmail.com>
2024-10-11 01:14:31 +0900

    [CHORE] 영양제 추천 오류날거 대비해서 그냥 추천 안 들어오면 없다고 뜨게 했어요,,

--- Front-End/dogbogam-front/src/pages/AIprediction/components/RecommendNutrient.tsx
+++ Front-End/dogbogam-front/src/pages/AIprediction/components/RecommendNutrient.tsx
@@ -12,19 +12,29 @@ export const RecommendNutrient = (props: { nutrient: recommendSupplement }) => {
         필요하신가요?
       </p>
       <p className="text-gray-400 font-medium text-sm mb-5">
-        AI 리포트를 기반으로 한 추천 보험이에요
+        AI 리포트를 기반으로 한 추천 영양제에요
       </p>
       <div className="border border-gray-200 rounded-lg shadow-lg flex justify-between place-items-end p-5">
-        <div className="flex space-x-5 place-items-center">
-          <img src={props.nutrient.imageUrl} alt="" className="h-[70px]" />
-          <div className="space-y-1">
-            <p className="font-semibold">{props.nutrient.productName}</p>
-            <p className="text-gray-500 text-sm">
-              {props.nutrient.price.toLocaleString()}원
-            </p>
+        {props.nutrient.imageUrl === "" ? (
+          // 영양제가 없을 경우
+          <div className="flex justify-center items-center w-full">
+            <p className="text-gray-500 text-md">맞춤 영양제가 없어요</p>
           </div>
-        </div>
-        <FontAwesomeIcon icon={faArrowRight} />
+        ) : (
+          // 영양제가 있을 경우
+          <>
+            <div className="flex space-x-5 place-items-center">
+              <img src={props.nutrient.imageUrl} alt="" className="h-[70px]" />
+              <div className="space-y-1">
+                <p className="font-semibold">{props.nutrient.productName}</p>
+                <p className="text-gray-500 text-sm">
+                  {props.nutrient.price.toLocaleString()}원
+                </p>
+              </div>
+            </div>
+            <FontAwesomeIcon icon={faArrowRight} />
+          </>
+        )}
       </div>
     </div>
   );
